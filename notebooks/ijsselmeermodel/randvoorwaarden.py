29a30097bd8edfbc340d08e0a0b85a23a1925ecf
Daniel <daniel@d2hydro.nl>
2023-11-07 12:18:02 +0100

    push model

--- notebooks/ijsselmeermodel/randvoorwaarden.py
+++ notebooks/ijsselmeermodel/randvoorwaarden.py
@@ -9,7 +9,11 @@ MODEL_DIR = Path(os.getenv("RIBASIM_NL_MODEL_DIR")) / "ijsselmeer"
 
 
 data = [
-    {"user_id": "WADDENZEE", "level": 0.0, "geometry": Point(136402.663, 563546.430)}
+    {
+        "user_id": "NL.WBHCODE.80.basin.WADDENZEE",
+        "level": 0.0,
+        "geometry": Point(136402.663, 563546.430),
+    }
 ]
 
 
