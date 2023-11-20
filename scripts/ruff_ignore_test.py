ea7be320f37a785b6c71e10d28ce52533e2b3a1c
Daniel <daniel@d2hydro.nl>
2023-11-20 11:28:26 +0100

    scripts cleaned

--- scripts/ruff_ignore_test.py
+++
@@ -1,9 +0,0 @@
-# %%
-"""Test if ruff ignores this file in pre-commit"""
-import pandas as pd
-
-df = pd.DataFrame([1, 2, 3])
-
-values = df[0].values  # should be deprecated as to_numpy() is the norm now
-
-# %%
