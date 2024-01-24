1678d1d21a61b4432fb4f3dead5f1e35815b1ca1
Daniel <daniel@d2hydro.nl>
2024-01-24 15:16:20 +0100

    Update kunstwerk.py

--- notebooks/rijkswaterstaat/kunstwerk.py
+++ notebooks/rijkswaterstaat/kunstwerk.py
@@ -11,6 +11,14 @@ from ribasim_nl import CloudStorage
 
 cloud = CloudStorage()
 
+
+def photo_url(code):
+    if code in kwk_media.index:
+        return kwk_media.at[code, "photo_url"]  # noqa: PD008
+    else:
+        return r"https://www.hydrobase.nl/static/icons/photo_placeholder.png"
+
+
 # File paths
 krw_lichaam = cloud.joinpath(
     "Basisgegevens", "KRW", "krw_oppervlaktewaterlichamen_nederland_vlakken.gpkg"
@@ -26,6 +34,10 @@ nis_hwvn = cloud.joinpath(
 primaire_kunstwerken = cloud.joinpath(
     "Rijkswaterstaat", "aangeleverd", "kunstwerken_primaire_waterkeringen.gpkg"
 )
+
+# load media
+kwk_media = pd.read_csv(cloud.joinpath("Rijkswaterstaat", "verwerkt", "kwk_media.csv"))
+kwk_media.set_index("code", inplace=True)
 
 # Load GeoDataFrames
 (
@@ -86,13 +98,50 @@ filtered_nearest_points = filtered_nearest_points[
 filtered_nearest_points = filtered_nearest_points.drop(["index_right"], axis=1)
 
 # Additional points
+
+
+# %%
+def name_from_baseline(string):
+    last_underscore_index = string.rfind("_")
+    if last_underscore_index != -1:
+        last_underscore_index += 1
+        return string[last_underscore_index:]
+    else:
+        return string
+
+
+baseline_kwk_add_gdf = baseline_kunstwerken_gdf[
+    baseline_kunstwerken_gdf["NAME"].isin(
+        [
+            "DM_68.30_R_US_Reevespuisluis",
+            "KR_C_HK_Kadoelen",
+        ]
+    )
+]
+
+baseline_kwk_add_gdf.loc[:, ["geometry"]] = baseline_kwk_add_gdf.centroid
+baseline_kwk_add_gdf["kw_naam"] = baseline_kwk_add_gdf["NAME"].apply(name_from_baseline)
+baseline_kwk_add_gdf["bron"] = "baseline"
+baseline_kwk_add_gdf["complex_naam"] = baseline_kwk_add_gdf["kw_naam"]
+
+# %%
+
+primaire_kwk_add_gdf = primaire_kunstwerken_gdf[
+    primaire_kunstwerken_gdf["kd_code1"].isin(
+        ["KD.32.gm.015", "KD.v2.ks.001", "KD.44.gm.001"]
+    )
+]
+
+primaire_kwk_add_gdf.rename(
+    columns={"kd_code1": "kw_code", "kd_naam": "kw_naam", "naam_compl": "complex_naam"},
+    inplace=True,
+)
+
+primaire_kwk_add_gdf["bron"] = "kunsterken primaire waterkeringen"
+
 additional_points = pd.concat(
     [
-        primaire_kunstwerken_gdf[
-            primaire_kunstwerken_gdf["kd_code1"].isin(
-                ["KD.32.gm.015", "KD.v2.ks.001", "KD.44.gm.001"]
-            )
-        ],
+        primaire_kwk_add_gdf,
         nis_hws_gdf[
             nis_hws_gdf["complex_code"].isin(
                 ["49D-400", "44D-002", "58C-001", "45B-352", "33F-001", "21G-350"]
@@ -103,16 +152,10 @@ additional_points = pd.concat(
                 ["49D-400", "44D-002", "58C-001", "45B-352", "33F-001", "21G-350"]
             )
         ],
-        baseline_kunstwerken_gdf[
-            baseline_kunstwerken_gdf["NAME"].isin(
-                [
-                    "DM_68.30_R_US_Reevespuisluis",
-                    "KR_C_HK_Kadoelen",
-                ]
-            )
-        ],
+        baseline_kwk_add_gdf,
     ]
 )
+
 
 add_baseline = gpd.sjoin_nearest(
     baseline_kunstwerken_gdf, filtered_nearest_points, how="inner", max_distance=1000
@@ -120,18 +163,23 @@ add_baseline = gpd.sjoin_nearest(
 
 
 # Concatenate additional points
-final_filtered_points = pd.concat(
-    [filtered_nearest_points, additional_points, add_baseline]
+final_filtered_points = pd.concat([filtered_nearest_points, additional_points])
+
+final_filtered_points.rename(
+    columns={"kw_naam": "naam", "kw_code": "code"}, inplace=True
 )
 
+final_filtered_points.loc[final_filtered_points["bron"].isna(), ["bron"]] = "NIS"
 # Add the lines from baseline_in_model to final_filtered_points
 
+# add photo_url
+final_filtered_points.loc[:, ["photo_url"]] = final_filtered_points["code"].apply(
+    photo_url
+)
 
 # Save results to GeoPackage files
-output_file = cloud.joinpath(
-    "Rijkswaterstaat", "verwerkt", "kunstwerken_select_legger.gpkg"
-)
-final_filtered_points.to_file(output_file, driver="GPKG")
+output_file = cloud.joinpath("Rijkswaterstaat", "verwerkt", "hydamo.gpkg")
+final_filtered_points.to_file(output_file, layer="kunstwerken", driver="GPKG")
 
 
 # %%
