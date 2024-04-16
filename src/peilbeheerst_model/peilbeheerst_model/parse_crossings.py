e4f02a91d27bbf52e9abfabae564ad9cecbba673
Guy-HKV <75747556+guydup@users.noreply.github.com>
2024-04-16 12:09:57 +0200

    ensure ring_buffer intersection

--- src/peilbeheerst_model/peilbeheerst_model/parse_crossings.py
+++ src/peilbeheerst_model/peilbeheerst_model/parse_crossings.py
@@ -675,6 +675,7 @@ class ParseCrossings:
         df_conn = df_conn.explode(index_parts=False)
         df_conn = df_conn[df_conn.geom_type == "LineString"].copy()
         line = MultiLineString(df_conn.geometry.tolist())
+        line = line.intersection(ring_buffer)
         line_ids = self.list_sep.join(sorted(df_conn.globalid.unique()))
 
         # Try and merge the MultiLineString to a single LineString
