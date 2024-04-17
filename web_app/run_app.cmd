e80c3335e8159e6ae4211795dfd5ab56a83099b2
DanielTollenaar <daniel@d2hydro.nl>
2024-04-17 13:41:13 +0200

    Update run_app.cmd

--- web_app/run_app.cmd
+++ web_app/run_app.cmd
@@ -1,5 +1,5 @@
-SET VIRTUAL_ENV=d:/repositories/Ribasim-NL/.pixi/envs/default
+SET VIRTUAL_ENV=../.pixi/envs/default
 SET PATH=%VIRTUAL_ENV%;%VIRTUAL_ENV%\Library\mingw-w64\bin;%VIRTUAL_ENV%\Library\usr\bin;%VIRTUAL_ENV%\Library\bin;%VIRTUAL_ENV%\Scripts;%VIRTUAL_ENV%\bin;%PATH%
 SET PROJ_LIB=%VIRTUAL_ENV%\Library\share\proj
 
-python -m bokeh serve app --show --dev --port 5007 --allow-websocket-origin=* --args "d:\projecten\D2306.LHM_RIBASIM\02.brongegevens\Rijkswaterstaat\modellen\hws\hws.toml"
+python -m bokeh serve app --show --dev --port 5007 --allow-websocket-origin=* --args %1
