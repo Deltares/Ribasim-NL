69fca1e873dd56662aca02dd665b31caa11b5d25
Daniel Tollenaar <daniel@d2hydro.nl>
2024-10-18 09:07:37 +0200

    fix aam

---
+++ notebooks/aa_en_maas/01_fix_model_network.py
@@ -0,0 +1,107 @@
+# %%
+import numpy as np
+import pandas as pd
+from ribasim.nodes import basin, level_boundary, manning_resistance, outlet
+from ribasim_nl import CloudStorage, Model, NetworkValidator
+
+cloud = CloudStorage()
+
+authority = "AaenMaas"
+short_name = "aam"
+
+ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_2024_6_3", f"{short_name}.toml")
+database_gpkg = ribasim_toml.with_name("database.gpkg")
+
+
+# %% read model
+model = Model.read(ribasim_toml)
+ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_fix_model_network", f"{short_name}.toml")
+network_validator = NetworkValidator(model)
+
+# %% some stuff we'll need again
+manning_data = manning_resistance.Static(length=[100], manning_n=[0.04], profile_width=[10], profile_slope=[1])
+level_data = level_boundary.Static(level=[0])
+
+basin_data = [
+    basin.Profile(level=[0.0, 1.0], area=[0.01, 1000.0]),
+    basin.Static(
+        drainage=[0.0],
+        potential_evaporation=[0.001 / 86400],
+        infiltration=[0.0],
+        precipitation=[0.005 / 86400],
+    ),
+    basin.State(level=[0]),
+]
+outlet_data = outlet.Static(flow_rate=[100])
+
+
+# HIER KOMEN ISSUES
+
+# Verwijderen duplicate edges
+
+# model.edge.df.drop_duplicates(inplace=True)
+
+# EINDE ISSUES
+
+
+## UPDATEN STATIC TABLES
+
+# %%
+# basin-profielen/state updaten
+df = pd.DataFrame(
+    {
+        "node_id": np.repeat(model.basin.node.df.index.to_numpy(), 2),
+        "level": [0.0, 1.0] * len(model.basin.node.df),
+        "area": [0.01, 1000.0] * len(model.basin.node.df),
+    }
+)
+df.index.name = "fid"
+model.basin.profile.df = df
+
+df = model.basin.profile.df.groupby("node_id")[["level"]].max().reset_index()
+df.index.name = "fid"
+model.basin.state.df = df
+
+# %%
+# tabulated_rating_curves updaten
+df = pd.DataFrame(
+    {
+        "node_id": np.repeat(model.tabulated_rating_curve.node.df.index.to_numpy(), 2),
+        "level": [0.0, 5] * len(model.tabulated_rating_curve.node.df),
+        "flow_rate": [0, 0.1] * len(model.tabulated_rating_curve.node.df),
+    }
+)
+df.index.name = "fid"
+model.tabulated_rating_curve.static.df = df
+
+
+# %%
+
+# level_boundaries updaten
+df = pd.DataFrame(
+    {
+        "node_id": model.level_boundary.node.df.index.to_list(),
+        "level": [0.0] * len(model.level_boundary.node.df),
+    }
+)
+df.index.name = "fid"
+model.level_boundary.static.df = df
+
+# %%
+# manning_resistance updaten
+length = len(model.manning_resistance.node.df)
+df = pd.DataFrame(
+    {
+        "node_id": model.manning_resistance.node.df.index.to_list(),
+        "length": [100.0] * length,
+        "manning_n": [100.0] * length,
+        "profile_width": [100.0] * length,
+        "profile_slope": [100.0] * length,
+    }
+)
+df.index.name = "fid"
+model.manning_resistance.static.df = df
+
+#  %% write model
+model.use_validation = True
+model.write(ribasim_toml)
