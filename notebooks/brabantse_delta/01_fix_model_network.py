0be553d791e48d8a192d077066f5362dc6bbec49
Daniel Tollenaar <daniel@d2hydro.nl>
2024-10-21 21:53:32 +0200

    fix wbd

--- notebooks/brabantse_delta/01_fix_model_network.py
+++ notebooks/brabantse_delta/01_fix_model_network.py
@@ -1,9 +1,12 @@
 # %%
+import geopandas as gpd
 import numpy as np
 import pandas as pd
+from ribasim import Node
 from ribasim.nodes import basin, level_boundary, manning_resistance, outlet
 
 from ribasim_nl import CloudStorage, Model, NetworkValidator
+from ribasim_nl.geometry import drop_z
 
 cloud = CloudStorage()
 
@@ -20,9 +23,9 @@ ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_fix_model_net
 network_validator = NetworkValidator(model)
 
 
-# hydroobject_gdf = gpd.read_file(
-#     cloud.joinpath(authority, "verwerkt", "4_ribasim", "hydamo.gpkg"), layer="hydroobject", fid_as_index=True
-# )
+hydroobject_gdf = gpd.read_file(
+    cloud.joinpath(authority, "verwerkt", "4_ribasim", "hydamo.gpkg"), layer="hydroobject", fid_as_index=True
+)
 
 
 # %% some stuff we'll need again
@@ -44,10 +47,35 @@ outlet_data = outlet.Static(flow_rate=[100])
 
 # HIER KOMEN ISSUES
 
-# %%
+# %% https://github.com/Deltares/Ribasim-NL/issues/152#issuecomment-2427492528
 
-# Verwijderen duplicate edges
-# model.edge.df.drop_duplicates(inplace=True)
+# Herstellen verbinding Schelde-Rijnverbinding met KDU02582
+model.remove_node(2288, remove_edges=True)
+model.redirect_edge(edge_id=2450, to_node_id=955)
+
+# Omkeren edges
+edge_ids = [2470, 2468, 2469, 2465, 748, 2476, 2489, 697, 2500, 2487, 2440]
+
+for edge_id in edge_ids:
+    model.reverse_edge(edge_id=edge_id)
+
+# toeoegen Donge
+basin_node = model.basin.add(
+    Node(geometry=drop_z(hydroobject_gdf.at[13091, "geometry"].boundary.geoms[0])), tables=basin_data
+)
+outlet_node = model.outlet.add(
+    Node(geometry=drop_z(hydroobject_gdf.at[13136, "geometry"].boundary.geoms[0])), tables=[outlet_data]
+)
+model.redirect_edge(edge_id=2477, from_node_id=basin_node.node_id, to_node_id=973)
+model.edge.add(basin_node, outlet_node)
+model.edge.add(outlet_node, model.level_boundary[31])
+
+outlet_node = model.outlet.add(
+    Node(geometry=drop_z(hydroobject_gdf.at[13088, "geometry"].boundary.geoms[0])), tables=[outlet_data]
+)
+model.redirect_edge(edge_id=2497, to_node_id=outlet_node.node_id)
+model.redirect_edge(edge_id=2498, from_node_id=outlet_node.node_id, to_node_id=basin_node.node_id)
+
 
 # EINDE ISSUES
 
@@ -55,15 +83,15 @@ outlet_data = outlet.Static(flow_rate=[100])
 # %%
 # corrigeren knoop-topologie
 
-# # ManningResistance bovenstrooms LevelBoundary naar Outlet
-# for row in network_validator.edge_incorrect_type_connectivity().itertuples():
-#     model.update_node(row.from_node_id, "Outlet", data=[outlet_data])
+# ManningResistance bovenstrooms LevelBoundary naar Outlet
+for row in network_validator.edge_incorrect_type_connectivity().itertuples():
+    model.update_node(row.from_node_id, "Outlet", data=[outlet_data])
 
-# # Inlaten van ManningResistance naar Outlet
-# for row in network_validator.edge_incorrect_type_connectivity(
-#     from_node_type="LevelBoundary", to_node_type="ManningResistance"
-# ).itertuples():
-#     model.update_node(row.to_node_id, "Outlet", data=[outlet_data])
+# Inlaten van ManningResistance naar Outlet
+for row in network_validator.edge_incorrect_type_connectivity(
+    from_node_type="LevelBoundary", to_node_type="ManningResistance"
+).itertuples():
+    model.update_node(row.to_node_id, "Outlet", data=[outlet_data])
 
 
 ## UPDATEN STATIC TABLES
@@ -138,7 +166,7 @@ model.flow_boundary.static.df = df
 
 
 #  %% write model
-model.use_validation = False
+model.use_validation = True
 model.write(ribasim_toml)
 
 # %%
