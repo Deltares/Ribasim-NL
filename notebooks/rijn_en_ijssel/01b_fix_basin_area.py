25130b0e9563c0448dee2ccd3dc22705c17c7d8e
ngoorden <49673619+ngoorden@users.noreply.github.com>
2024-11-28 10:24:22 +0100

    fix basins renij

--- notebooks/rijn_en_ijssel/01b_fix_basin_area.py
+++ notebooks/rijn_en_ijssel/01b_fix_basin_area.py
@@ -17,6 +17,10 @@ ribasim_model_path = ribasim_model_dir / f"{model_short_name}.toml"
 model = Model.read(ribasim_model_path)
 network_validator = NetworkValidator(model)
 
+# Load Ribasim areas
+ribasim_areas_path = cloud_storage.joinpath(authority_name, "verwerkt", "4_ribasim", "areas.gpkg")
+ribasim_areas_gdf = gpd.read_file(ribasim_areas_path, fid_as_index=True, layer="areas")
+
 # Load node edit data
 model_edits_url = cloud_storage.joinurl(authority_name, "verwerkt", "model_edits.gpkg")
 model_edits_path = cloud_storage.joinpath(authority_name, "verwerkt", "model_edits.gpkg")
@@ -38,16 +42,43 @@ for action in gpd.list_layers(model_edits_path).name:
 # remove unassigned basin area
 model.remove_unassigned_basin_area()
 
-# %% corrigeren knoop-topologie
-# ManningResistance bovenstrooms LevelBoundary naar Outlet
-for row in network_validator.edge_incorrect_type_connectivity().itertuples():
-    model.update_node(row.from_node_id, "Outlet")
 
-# Inlaten van ManningResistance naar Outlet
-for row in network_validator.edge_incorrect_type_connectivity(
-    from_node_type="LevelBoundary", to_node_type="ManningResistance"
-).itertuples():
-    model.update_node(row.to_node_id, "Outlet")
+# %% Assign Ribasim model ID's (dissolved areas) to the model basin areas (original areas with code) by overlapping the Ribasim area file baed on largest overlap
+# then assign Ribasim node-ID's to areas with the same area code. Many nodata areas disappear by this method
+# Create the overlay of areas
+ribasim_areas_gdf = ribasim_areas_gdf.to_crs(model.basin.area.df.crs)
+combined_basin_areas_gdf = gpd.overlay(ribasim_areas_gdf, model.basin.area.df, how="union").explode()
+combined_basin_areas_gdf["geometry"] = combined_basin_areas_gdf["geometry"].apply(lambda x: x if x.has_z else x)
+
+# Calculate area for each geometry
+combined_basin_areas_gdf["area"] = combined_basin_areas_gdf.geometry.area
+
+# Separate rows with and without node_id
+non_null_basin_areas_gdf = combined_basin_areas_gdf[combined_basin_areas_gdf["node_id"].notna()]
+
+# Find largest area node_ids for each code
+largest_area_node_ids = non_null_basin_areas_gdf.loc[
+    non_null_basin_areas_gdf.groupby("code")["area"].idxmax(), ["code", "node_id"]
+]
+
+# Merge largest area node_ids back into the combined DataFrame
+combined_basin_areas_gdf = combined_basin_areas_gdf.merge(
+    largest_area_node_ids, on="code", how="left", suffixes=("", "_largest")
+)
+
+# Fill missing node_id with the largest_area node_id
+combined_basin_areas_gdf["node_id"] = combined_basin_areas_gdf["node_id"].fillna(
+    combined_basin_areas_gdf["node_id_largest"]
+)
+combined_basin_areas_gdf.drop(columns=["node_id_largest"], inplace=True)
+combined_basin_areas_gdf = combined_basin_areas_gdf.drop_duplicates()
+combined_basin_areas_gdf = combined_basin_areas_gdf.dissolve(by="node_id").reset_index()
+combined_basin_areas_gdf = combined_basin_areas_gdf[["node_id", "geometry"]]
+combined_basin_areas_gdf.index.name = "fid"
+
+# %%
+model.basin.area.df = combined_basin_areas_gdf
+
 
 # %%
 model.use_validation = True
