1facdf7a302de458b83557fe1edb11929cbbaff6
Daniel Tollenaar <daniel@d2hydro.nl>
2024-11-30 13:12:10 +0100

    fix vlakken hea

--- notebooks/hunze_en_aas/01_fix_model_network.py
+++ notebooks/hunze_en_aas/01_fix_model_network.py
@@ -1,4 +1,7 @@
 # %%
+import inspect
+
+import geopandas as gpd
 import numpy as np
 import pandas as pd
 from ribasim import Node
@@ -18,6 +21,12 @@ database_gpkg = ribasim_toml.with_name("database.gpkg")
 model = Model.read(ribasim_toml)
 ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_fix_model_network", f"{short_name}.toml")
 network_validator = NetworkValidator(model)
+
+# Load node edit data
+model_edits_url = cloud.joinurl(authority, "verwerkt", "model_edits.gpkg")
+model_edits_path = cloud.joinpath(authority, "verwerkt", "model_edits.gpkg")
+if not model_edits_path.exists():
+    cloud.download_file(model_edits_url)
 
 
 # %% some stuff we'll need again
@@ -55,12 +64,10 @@ for row in basin_nodes_df.itertuples():
     basin_node = model.basin.add(Node(geometry=row.geometry), tables=basin_data)
 
     # update edge_table
-    model.edge.df.loc[basin_edges_df[basin_edges_df.from_node_id == row.node_id].index, ["from_node_id"]] = (
-        basin_node.node_id
-    )
-    model.edge.df.loc[basin_edges_df[basin_edges_df.to_node_id == row.node_id].index, ["to_node_id"]] = (
-        basin_node.node_id
-    )
+    mask = (basin_edges_df.from_node_id == row.node_id) & (basin_edges_df.distance(row.geometry) < 0.1)
+    model.edge.df.loc[basin_edges_df[mask].index, ["from_node_id"]] = basin_node.node_id
+    mask = (basin_edges_df.to_node_id == row.node_id) & (basin_edges_df.distance(row.geometry) < 0.1)
+    model.edge.df.loc[basin_edges_df[mask].index, ["to_node_id"]] = basin_node.node_id
 
 
 # EINDE ISSUES
@@ -70,14 +77,14 @@ for row in basin_nodes_df.itertuples():
 # corrigeren knoop-topologie
 
 # ManningResistance bovenstrooms LevelBoundary naar Outlet
-# for row in network_validator.edge_incorrect_type_connectivity().itertuples():
-#     model.update_node(row.from_node_id, "Outlet", data=[outlet_data])
+for row in network_validator.edge_incorrect_type_connectivity().itertuples():
+    model.update_node(row.from_node_id, "Outlet", data=[outlet_data])
 
-# # Inlaten van ManningResistance naar Outlet
-# for row in network_validator.edge_incorrect_type_connectivity(
-#     from_node_type="LevelBoundary", to_node_type="ManningResistance"
-# ).itertuples():
-#     model.update_node(row.to_node_id, "Outlet", data=[outlet_data])
+# Inlaten van ManningResistance naar Outlet
+for row in network_validator.edge_incorrect_type_connectivity(
+    from_node_type="LevelBoundary", to_node_type="ManningResistance"
+).itertuples():
+    model.update_node(row.to_node_id, "Outlet", data=[outlet_data])
 
 
 ## UPDATEN STATIC TABLES
@@ -163,8 +170,35 @@ df.index.name = "fid"
 model.outlet.static.df = df
 
 
+# %%
+actions = [
+    "remove_node",
+    "add_basin",
+    "add_basin_area",
+    "update_basin_area",
+    "merge_basins",
+    "reverse_edge",
+    "move_node",
+    "connect_basins",
+    "update_node",
+    "deactivate_node",
+]
+actions = [i for i in actions if i in gpd.list_layers(model_edits_path).name.to_list()]
+for action in actions:
+    print(action)
+    # get method and args
+    method = getattr(model, action)
+    keywords = inspect.getfullargspec(method).args
+    df = gpd.read_file(model_edits_path, layer=action, fid_as_index=True)
+    for row in df.itertuples():
+        # filter kwargs by keywords
+        kwargs = {k: v for k, v in row._asdict().items() if k in keywords}
+        method(**kwargs)
+
+
 #  %% write model
-model.use_validation = False
+model.use_validation = True
 model.write(ribasim_toml)
+model.invalid_topology_at_node().to_file(ribasim_toml.with_name("invalid_topology_at_connector_nodes.gpkg"))
 
 # %%
