88ce228d74fb153f81fcb224d27beb85ca6fba81
Daniel Tollenaar <daniel@d2hydro.nl>
2025-02-04 11:59:18 +0100

    meer data

--- notebooks/aa_en_maas/02_prepare_parameters.py
+++ notebooks/aa_en_maas/02_prepare_parameters.py
@@ -1,7 +1,9 @@
 # %%
+import geopandas as gpd
 
 from ribasim_nl import CloudStorage, Model
 from ribasim_nl.parametrization.static_data_xlsx import StaticData
+from ribasim_nl.parametrization.target_level import upstream_target_levels
 from ribasim_nl.streefpeilen import add_streefpeil
 
 cloud = CloudStorage()
@@ -12,6 +14,15 @@ short_name = "aam"
 ribasim_dir = cloud.joinpath(authority, "modellen", f"{authority}_fix_model")
 ribasim_toml = ribasim_dir / f"{short_name}.toml"
 
+static_data_xlsx = cloud.joinpath(
+    authority,
+    "verwerkt",
+    "parameters",
+    "static_data_template.xlsx",
+)
+
+
+stuwen_shp = cloud.joinpath(authority, "verwerkt", "1_ontvangen_data", "Na_levering_20240418", "stuwen.shp")
 model = Model.read(ribasim_toml)
 
 
@@ -30,15 +41,23 @@ _ = add_streefpeil(
 
 # build static_data.xlsx
 
-# defaults
-static_data_xlsx = cloud.joinpath(
-    authority,
-    "verwerkt",
-    "parameters",
-    "static_data_template.xlsx",
-)
-
+# init static data from model and defaults
 static_data = StaticData(model=model, xlsx_path=static_data_xlsx)
+
+# find min_upstream_level in stuwen
+min_upstream_level = gpd.read_file(stuwen_shp).set_index("CODE")["WS_STREE_2"]
+min_upstream_level = min_upstream_level[min_upstream_level != 0]
+min_upstream_level.name = "min_upstream_level"
+min_upstream_level.index.name = "code"
+static_data.add_series(node_type="Outlet", series=min_upstream_level)
+
+# find min_upstream_level in gemalen
+static_data.reset_data_frame(node_type="Pump")
+min_upstream_level = upstream_target_levels(model=model, node_ids=static_data.pump.node_id)
+min_upstream_level = min_upstream_level[min_upstream_level.notna()]
+min_upstream_level.name = "min_upstream_level"
+static_data.add_series(node_type="Pump", series=min_upstream_level)
+
 static_data.write()
 # %%
 
--- src/ribasim_nl/ribasim_nl/parametrization/static_data_xlsx.py
+++ src/ribasim_nl/ribasim_nl/parametrization/static_data_xlsx.py
@@ -4,10 +4,39 @@ from pathlib import Path
 import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles import Border
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from ribasim_nl import Model
+from ribasim_nl.case_conversions import pascal_to_snake_case
 from ribasim_nl.parametrization.empty_table import empty_table_df
+
+description = [
+    {"sheet": "defaults", "beschrijving": "default settings bij geen-data"},
+    {"sheet": "Outlet & Pump", "beschrijving": "gemodelleerde instellingen bij RIBASIM Pump en Outlet nodes"},
+    {"kolom": "flow_rate", "beschrijving": "gemodelleerde capaciteit (m3/s) van het kunstwerk"},
+    {
+        "kolom": "code",
+        "beschrijving": "code waterbeheerder van het object",
+    },
+    {
+        "kolom": "name",
+        "beschrijving": "naam van het object",
+    },
+    {
+        "kolom": "flow_rate_mm_per_day",
+        "beschrijving": "gemodelleerde capaciteit (mm/dag) van het kunstwerk t.o.v. voor het bovenstrooms/benedenstroomse (uitlaat/inlaat) stroomgebied",
+    },
+    {
+        "kolom": "categorie",
+        "beschrijving": "categorie van het kunstwerk. Is de logische link tussen de Pump/Outlet en defaults table.",
+    },
+    {
+        "kolom": "opmerking_waterbeheerder",
+        "beschrijving": "een opmerking van de waterbeheerder ter duiding van de ingevulde waarde(n)",
+    },
+    {"kolom": "upstream_level_offset", "beschrijving": "afslagpunt (meter) boven bovenstrooms streefpeil"},
+    {"kolom": "downstream_level_offset", "beschrijving": "afslagpunt (meter) onder benedenstrooms streefpeil"},
+]
 
 defaults = {
     "Afvoergemaal": {
@@ -44,49 +73,89 @@ defaults = {
 class StaticData(BaseModel):
     xlsx_path: os.PathLike
     model: Model
-    defaults: dict = defaults
+    default_dict: dict = defaults
+    description_list: list = description
+    outlet: pd.DataFrame | None = None
+    pump: pd.DataFrame | None = None
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
     def __post_init__(self):
         self.xlsx_path = Path(self.xlsx_path)
 
     @property
-    def defaults_df(self):
-        df = pd.DataFrame.from_dict(self.defaults, orient="index")
+    def defaults(self):
+        df = pd.DataFrame.from_dict(self.default_dict, orient="index")
         df.index.name = "categorie"
         return df
 
+    @property
+    def description(self):
+        df = pd.DataFrame(self.description_list)
+        df.sort_values(by=["sheet", "kolom"], inplace=True)
+        return df[["sheet", "kolom", "beschrijving"]]
+
+    def reset_data_frame(self, node_type):
+        columns = ["node_id", "name", "code", "flow_rate", "min_upstream_level", "max_downstream_level"]
+        extra_columns = ["categorie", "opmerking_waterbeheerder"]
+        df = empty_table_df(
+            model=self.model,
+            table_type="Static",
+            node_type=node_type,
+            meta_columns=["meta_code_waterbeheerder", "name"],
+        )
+        df.rename(columns={"meta_code_waterbeheerder": "code"}, inplace=True)
+        if node_type == "Pump":
+            df["categorie"] = "Afvoergemaal"
+        if node_type == "Outlet":
+            df["categorie"] = "Uitlaat"
+        df["opmerking_waterbeheerder"] = ""
+        df = df[columns + extra_columns]
+        setattr(self, pascal_to_snake_case(node_type), df)
+        return getattr(self, pascal_to_snake_case(node_type))
+
+    def add_series(self, node_type, series):
+        col = series.index.name
+
+        # get dataframe
+        df = getattr(self, pascal_to_snake_case(node_type))
+        if df is None:
+            df = self.reset_data_frame(node_type=node_type)
+
+        # sanitize series to existing codes in node_type
+        series = series[series.index.isin(df[col])]
+
+        # mask table where row in series and set data
+        mask = df[col].isin(series.index)
+        df.loc[mask, series.name] = series[df[mask][col]].to_numpy()
+
     def write(self):
+        # remove exel if exists
         if self.xlsx_path.exists():
             self.xlsx_path.unlink()
         else:
             self.xlsx_path.parent.mkdir(exist_ok=True, parents=True)
 
-        # write defaults
-        self.defaults_df.to_excel(self.xlsx_path, sheet_name="defaults")
-
         # write sheets
+        self.description.to_excel(self.xlsx_path, sheet_name="beschrijving", index=False)
         with pd.ExcelWriter(self.xlsx_path, mode="a", if_sheet_exists="replace") as xlsx_writer:
-            columns = ["node_id", "name", "code", "flow_rate", "min_upstream_level", "max_downstream_level"]
-            extra_columns = ["categorie", "opmerking_waterbeheerder"]
+            self.defaults.to_excel(xlsx_writer, sheet_name="defaults")
             for node_type in ["Pump", "Outlet"]:
-                df = empty_table_df(
-                    model=self.model,
-                    table_type="Static",
-                    node_type=node_type,
-                    meta_columns=["meta_code_waterbeheerder", "name"],
-                )
-                df.rename(columns={"meta_code_waterbeheerder": "code"}, inplace=True)
-                if node_type == "Pump":
-                    df["categorie"] = "Afvoergemaal"
-                if node_type == "Outlet":
-                    df["categorie"] = "Uitlaat"
-                df["opmerking_waterbeheerder"] = ""
-                df[columns + extra_columns].to_excel(xlsx_writer, sheet_name=node_type, index=False)
+                df = getattr(self, pascal_to_snake_case(node_type))
+                if df is None:
+                    df = self.reset_data_frame(node_type=node_type)
+                df.to_excel(xlsx_writer, sheet_name=node_type, index=False)
 
         # fix formatting
         wb = load_workbook(self.xlsx_path)
         for sheet_name in pd.ExcelFile(self.xlsx_path).sheet_names:
             ws = wb[sheet_name]
+
+            # freeze panes
+            ws.freeze_panes = "A2"
+
+            # add filters
+            ws.auto_filter.ref = ws.dimensions
 
             # Remove cell borders
             for row in ws.iter_rows():
@@ -106,3 +175,4 @@ class StaticData(BaseModel):
 
             # Save the modified file
             wb.save(self.xlsx_path)
+        wb.close()
