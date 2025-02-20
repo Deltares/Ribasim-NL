e41f4e6a353584b4ab71a726a2d9eaea197cf5f2
Hendrickx <Hendrickx@HKV.nl>
2025-02-20 13:52:45 +0100

    Implementation of WF.

--- src/peilbeheerst_model/Parametrize/WetterskipFryslan_parametrize.py
+++ src/peilbeheerst_model/Parametrize/WetterskipFryslan_parametrize.py
@@ -6,6 +6,7 @@ import warnings
 
 import ribasim
 import ribasim.nodes
+from Parametrize.Rijnland_parametrize import aanvoergebieden
 from ribasim import Node
 from ribasim.nodes import level_boundary, pump, tabulated_rating_curve
 from shapely import Point
@@ -16,6 +17,8 @@ from peilbeheerst_model.add_storage_basins import AddStorageBasins
 from peilbeheerst_model.controle_output import Control
 from peilbeheerst_model.ribasim_feedback_processor import RibasimFeedbackProcessor
 from ribasim_nl import CloudStorage
+
+AANVOER_CONDITIONS: bool = True
 
 # model settings
 waterschap = "WetterskipFryslan"
@@ -221,8 +224,8 @@ add_storage_basins.create_bergende_basins()
 
 # set static forcing
 forcing_dict = {
-    "precipitation": ribasim_param.convert_mm_day_to_m_sec(10),
-    "potential_evaporation": ribasim_param.convert_mm_day_to_m_sec(0),
+    "precipitation": ribasim_param.convert_mm_day_to_m_sec(0 if AANVOER_CONDITIONS else 10),
+    "potential_evaporation": ribasim_param.convert_mm_day_to_m_sec(10 if AANVOER_CONDITIONS else 0),
     "drainage": ribasim_param.convert_mm_day_to_m_sec(0),
     "infiltration": ribasim_param.convert_mm_day_to_m_sec(0),
 }
@@ -242,10 +245,23 @@ ribasim_model.level_boundary.static.df.level = default_level
 # add outlet
 ribasim_param.add_outlets(ribasim_model, delta_crest_level=0.10)
 
+# prepare 'aanvoergebieden'
+if AANVOER_CONDITIONS:
+    aanvoergebieden = supply.special_load_geometry(
+        f_geometry=str(aanvoer_path),
+        method="extract",
+        layer="DAMO_W_AfvoergebiedAanvoergebied",
+        key="functieGebied",
+        value="2",
+    )
+else:
+    aanvoergebieden = None
+
 # add control, based on the meta_categorie
-ribasim_param.identify_node_meta_categorie(ribasim_model)
+ribasim_param.identify_node_meta_categorie(ribasim_model, aanvoer_enabled=AANVOER_CONDITIONS)
 ribasim_param.find_upstream_downstream_target_levels(ribasim_model, node="outlet")
 ribasim_param.find_upstream_downstream_target_levels(ribasim_model, node="pump")
+ribasim_param.set_aanvoer_flags(ribasim_model, aanvoergebieden, aanvoer_enabled=AANVOER_CONDITIONS)
 # ribasim_param.add_discrete_control(ribasim_model, waterschap, default_level)
 ribasim_param.determine_min_upstream_max_downstream_levels(ribasim_model, waterschap)
 
@@ -260,24 +276,6 @@ ribasim_model.manning_resistance.static.df.manning_n = 0.01
 # last formatting of the tables
 # only retain node_id's which are present in the .node table
 ribasim_param.clean_tables(ribasim_model, waterschap)
-
-# set 'aanvoer'-settings
-aanvoergebieden = supply.special_load_geometry(
-    f_geometry=str(aanvoer_path),
-    method="extract",
-    layer="DAMO_W_AfvoergebiedAanvoergebied",
-    key="functieGebied",
-    value="2",
-)
-
-# label basins as 'aanvoergebied'
-sb = supply.SupplyBasin(ribasim_model, aanvoergebieden)
-sb.exec()
-# label outlets as 'aanvoerkunstwerk'
-so = supply.SupplyOutlet(sb.model)
-so.exec()
-# reset ribasim model
-ribasim_model = so.model
 
 # set numerical settings
 # write model output
