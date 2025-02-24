fada6255e55eafd7b56722d968399c2607d539cb
Daniel Tollenaar <daniel@d2hydro.nl>
2025-02-24 15:24:10 +0100

    parameterized wbd

--- notebooks/brabantse_delta/02_prepare_model.py
+++ notebooks/brabantse_delta/02_prepare_model.py
@@ -1,5 +1,6 @@
 # %%
 import geopandas as gpd
+import pandas as pd
 
 from ribasim_nl import CloudStorage, Model, Network
 from ribasim_nl.link_geometries import fix_link_geometries
@@ -27,6 +28,7 @@ damo_profiles_gpkg = cloud.joinpath(authority, "verwerkt/profielen.gpkg")
 peilgebieden_path = cloud.joinpath(authority, "verwerkt/4_ribasim/hydamo.gpkg")
 top10NL_gpkg = cloud.joinpath("Basisgegevens", "Top10NL", "top10nl_Compleet.gpkg")
 
+
 cloud.synchronize(filepaths=[peilgebieden_path, top10NL_gpkg])
 
 # %% init things
@@ -38,6 +40,16 @@ static_data = StaticData(model=model, xlsx_path=static_data_xlsx)
 
 # %%
 
+network = Network(lines_gdf=gpd.read_file(hydamo_gpkg, layer="hydroobject"))
+damo_profiles = DAMOProfiles(
+    model=model,
+    network=network,
+    profile_line_df=gpd.read_file(damo_profiles_gpkg, layer="profiellijn"),
+    profile_point_df=gpd.read_file(damo_profiles_gpkg, layer="profielpunt"),
+    water_area_df=gpd.read_file(top10NL_gpkg, layer="top10nl_waterdeel_vlak"),
+    profile_line_id_col="code",
+)
+
 # fix link geometries
 if link_geometries_gpkg.exists():
     link_geometries_df = gpd.read_file(link_geometries_gpkg).set_index("edge_id")
@@ -46,16 +58,8 @@ if link_geometries_gpkg.exists():
         model.edge.df.loc[link_geometries_df.index, "meta_profielid_waterbeheerder"] = link_geometries_df[
             "meta_profielid_waterbeheerder"
         ]
+    profiles_df = gpd.read_file(profiles_gpkg).set_index("profiel_id")
 else:
-    network = Network(lines_gdf=gpd.read_file(hydamo_gpkg, layer="hydroobject"))
-    damo_profiles = DAMOProfiles(
-        model=model,
-        network=network,
-        profile_line_df=gpd.read_file(damo_profiles_gpkg, layer="profiellijn"),
-        profile_point_df=gpd.read_file(damo_profiles_gpkg, layer="profielpunt"),
-        water_area_df=gpd.read_file(top10NL_gpkg, layer="top10nl_waterdeel_vlak"),
-        profile_line_id_col="code",
-    )
     profiles_df = damo_profiles.process_profiles()
     profiles_df.to_file(profiles_gpkg)
     add_link_profile_ids(model, profiles=damo_profiles, id_col="code")
@@ -108,6 +112,20 @@ min_upstream_level = min_upstream_level[min_upstream_level.notna()]
 min_upstream_level.name = "min_upstream_level"
 static_data.add_series(node_type="Outlet", series=min_upstream_level)
 
+# from DAMO profiles
+node_ids = static_data.outlet[static_data.outlet.min_upstream_level.isna()].node_id.to_numpy()
+profile_ids = [
+    model.edge.df[model.edge.df.to_node_id == node_id].iloc[0]["meta_profielid_waterbeheerder"] for node_id in node_ids
+]
+levels = (
+    profiles_df.loc[profile_ids]["bottom_level"]
+    + (profiles_df.loc[profile_ids]["invert_level"] - profiles_df.loc[profile_ids]["bottom_level"]) / 2
+).to_numpy()
+
+min_upstream_level = pd.Series(levels, index=node_ids, name="min_upstream_level")
+min_upstream_level.index.name = "node_id"
+static_data.add_series(node_type="Outlet", series=min_upstream_level, fill_na=True)
+
 
 # %%
 
@@ -121,11 +139,72 @@ min_upstream_level = min_upstream_level[min_upstream_level.notna()]
 min_upstream_level.name = "min_upstream_level"
 static_data.add_series(node_type="Pump", series=min_upstream_level)
 
+# from DAMO profiles
+node_ids = static_data.pump[static_data.pump.min_upstream_level.isna()].node_id.to_numpy()
+profile_ids = [
+    model.edge.df[model.edge.df.to_node_id == node_id].iloc[0]["meta_profielid_waterbeheerder"] for node_id in node_ids
+]
+levels = (
+    profiles_df.loc[profile_ids]["bottom_level"]
+    + (profiles_df.loc[profile_ids]["invert_level"] - profiles_df.loc[profile_ids]["bottom_level"]) / 2
+).to_numpy()
+
+min_upstream_level = pd.Series(levels, index=node_ids, name="min_upstream_level")
+min_upstream_level.index.name = "node_id"
+static_data.add_series(node_type="Pump", series=min_upstream_level, fill_na=True)
+
 
 # %%
 
 # # BASIN
 static_data.reset_data_frame(node_type="Basin")
+
+# fill streefpeil from ds min_upstream_level
+node_ids = static_data.basin[static_data.basin.streefpeil.isna()].node_id.to_numpy()
+
+ds_node_ids = (model.downstream_node_id(i) for i in node_ids)
+ds_node_ids = [i.to_list() if isinstance(i, pd.Series) else [i] for i in ds_node_ids]
+ds_node_ids = pd.Series(ds_node_ids, index=node_ids).explode()
+
+ds_levels = pd.concat([static_data.basin, static_data.outlet], ignore_index=True).set_index("node_id")[
+    "min_upstream_level"
+]
+ds_levels.dropna(inplace=True)
+ds_levels = ds_levels[ds_levels.index.isin(ds_node_ids)]
+ds_node_ids = ds_node_ids[ds_node_ids.isin(ds_levels.index)]
+
+levels = ds_node_ids.apply(lambda x: ds_levels[x])
+streefpeil = levels.groupby(levels.index).min()
+streefpeil.name = "streefpeil"
+streefpeil.index.name = "node_id"
+static_data.add_series(node_type="Basin", series=streefpeil, fill_na=True)
+
+# get all nodata streefpeilen with their profile_ids and levels
+node_ids = static_data.basin[static_data.basin.streefpeil.isna()].node_id.to_numpy()
+profile_ids = [damo_profiles.get_profile_id(node_id) for node_id in node_ids]
+levels = (
+    profiles_df.loc[profile_ids]["bottom_level"]
+    + (profiles_df.loc[profile_ids]["invert_level"] - profiles_df.loc[profile_ids]["bottom_level"]) / 2
+).to_numpy()
+
+# # update static_data
+profielid = pd.Series(profile_ids, index=pd.Index(node_ids, name="node_id"), name="profielid")
+static_data.add_series(node_type="Basin", series=profielid, fill_na=True)
+streefpeil = pd.Series(levels, index=pd.Index(node_ids, name="node_id"), name="streefpeil")
+static_data.add_series(node_type="Basin", series=streefpeil, fill_na=True)
+
+# # update model basin-data
+model.basin.area.df.set_index("node_id", inplace=True)
+
+streefpeil = static_data.basin.set_index("node_id")["streefpeil"]
+model.basin.area.df.loc[streefpeil.index, "meta_streefpeil"] = streefpeil
+profiellijnid = static_data.basin.set_index("node_id")["profielid"]
+model.basin.area.df.loc[streefpeil.index, "meta_profiellijnid"] = profiellijnid
+
+model.basin.area.df.reset_index(drop=False, inplace=True)
+model.basin.area.df.index += 1
+model.basin.area.df.index.name = "fid"
+
 
 # %%
 
---
+++ notebooks/de_dommel/03_parameterize_model.py
@@ -0,0 +1,51 @@
+# %%
+import time
+
+from peilbeheerst_model.controle_output import Control
+from ribasim_nl import CloudStorage, Model
+
+cloud = CloudStorage()
+authority = "AaenMaas"
+short_name = "aam"
+
+run_model = True
+
+parameters_dir = static_data_xlsx = cloud.joinpath(authority, "verwerkt", "parameters")
+static_data_xlsx = parameters_dir / "static_data.xlsx"
+profiles_gpkg = parameters_dir / "profiles.gpkg"
+
+ribasim_dir = cloud.joinpath(authority, "modellen", f"{authority}_prepare_model")
+ribasim_toml = ribasim_dir / f"{short_name}.toml"
+
+# # you need the excel, but the model should be local-only by running 01_fix_model.py
+cloud.synchronize(filepaths=[static_data_xlsx, profiles_gpkg])
+cloud.synchronize(filepaths=[ribasim_dir], check_on_remote=False)
+
+# %%
+
+# read
+model = Model.read(ribasim_toml)
+
+start_time = time.time()
+# %%
+# parameterize
+model.parameterize(static_data_xlsx=static_data_xlsx, precipitation_mm_per_day=10, profiles_gpkg=profiles_gpkg)
+print("Elapsed Time:", time.time() - start_time, "seconds")
+
+# %%
+
+# Write model
+ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_parameterized_model", f"{short_name}.toml")
+model.write(ribasim_toml)
+
+# %%
+
+# run model
+if run_model:
+    exit_code = model.run()
+    assert exit_code == 0
+
+    # # %%
+    controle_output = Control(ribasim_toml=ribasim_toml)
+    indicators = controle_output.run_all()
+# %%
