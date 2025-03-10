a9b0a184f28fb17556b50f7d9688cf91ba0c5915
Daniel Tollenaar <daniel@d2hydro.nl>
2025-03-10 20:53:04 +0100

    hdsr_running

--- notebooks/stichtse_rijnlanden/03_parameterize_model.py
+++ notebooks/stichtse_rijnlanden/03_parameterize_model.py
@@ -1,15 +1,16 @@
 # %%
 import time
-from datetime import timedelta
+from pathlib import Path
 
 from peilbeheerst_model.controle_output import Control
 from ribasim_nl import CloudStorage, Model
+from ribasim_nl.check_basin_level import add_check_basin_level
 
 cloud = CloudStorage()
+ribasim_exe = Path(r"c:\\Ribasim_dev\\ribasim.exe")
 authority = "StichtseRijnlanden"
 short_name = "hdsr"
-run_model = True
-run_period = timedelta(days=10)
+run_model = False
 static_data_xlsx = cloud.joinpath(
     authority,
     "verwerkt",
@@ -18,6 +19,7 @@ static_data_xlsx = cloud.joinpath(
 )
 ribasim_dir = cloud.joinpath(authority, "modellen", f"{authority}_prepare_model")
 ribasim_toml = ribasim_dir / f"{short_name}.toml"
+qlr_path = cloud.joinpath("Basisgegevens\\QGIS_lyr\\output_controle_vaw_afvoer.qlr")
 
 # # you need the excel, but the model should be local-only by running 01_fix_model.py
 # cloud.synchronize(filepaths=[static_data_xlsx])
@@ -36,21 +38,38 @@ print("Elapsed Time:", time.time() - start_time, "seconds")
 
 # %%
 
+# fixes
+model.remove_node(node_id=1031, remove_edges=True)
+model.merge_basins(basin_id=1882, to_node_id=1867)
+model.merge_basins(basin_id=1888, to_node_id=1867)
+model.merge_basins(basin_id=1676, to_node_id=1375)
+model.merge_basins(basin_id=1866, to_node_id=1883)
+model.merge_basins(basin_id=1870, to_node_id=1883)
+model.merge_basins(basin_id=1695, to_node_id=1800)
+model.merge_basins(basin_id=2011, to_node_id=2050)
+model.merge_basins(basin_id=2049, to_node_id=2050)
+model.merge_basins(basin_id=1892, to_node_id=1947)
+model.merge_basins(basin_id=1834, to_node_id=1947)
+model.merge_basins(basin_id=2010, to_node_id=1863)
+
+# %% deactivate inlets
+node_ids = model.outlet.node.df[model.outlet.node.df.meta_code_waterbeheerder.str.startswith("I")].index.to_numpy()
+model.outlet.static.df.loc[model.outlet.static.df.node_id.isin(node_ids), "active"] = False
+model.solver.maxiters = 100000
 # Write model
+add_check_basin_level(model=model)
 ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_parameterized_model", f"{short_name}.toml")
+# model.endtime = model.starttime + timedelta(days=31)
 model.write(ribasim_toml)
 
 # %%
 
 # run model
 if run_model:
-    if run_period is not None:
-        model.endtime = model.starttime + run_period
-        model.write(ribasim_toml)
-    exit_code = model.run()
+    exit_code = model.run(ribasim_exe=ribasim_exe)
     assert exit_code == 0
 
 # %%
-controle_output = Control(ribasim_toml=ribasim_toml)
-indicators = controle_output.run_all()
+controle_output = Control(ribasim_toml=ribasim_toml, qlr_path=qlr_path)
+indicators = controle_output.run_afvoer()
 # %%
