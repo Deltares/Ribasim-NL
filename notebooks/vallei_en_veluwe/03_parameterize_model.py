c08e2d938f457fae815c9313b906be03d2ba5630
Daniel Tollenaar <daniel@d2hydro.nl>
2025-03-17 09:58:07 +0100

    control_manning

--- notebooks/vallei_en_veluwe/03_parameterize_model.py
+++ notebooks/vallei_en_veluwe/03_parameterize_model.py
@@ -15,6 +15,7 @@ static_data_xlsx = parameters_dir / "static_data_template.xlsx"
 profiles_gpkg = parameters_dir / "profiles.gpkg"
 ribasim_dir = cloud.joinpath(authority, "modellen", f"{authority}_prepare_model")
 ribasim_toml = ribasim_dir / f"{short_name}.toml"
+qlr_path = cloud.joinpath("Basisgegevens\\QGIS_lyr\\output_controle_vaw_afvoer.qlr")
 
 # # you need the excel, but the model should be local-only by running 01_fix_model.py
 # cloud.synchronize(fil
@@ -47,7 +48,7 @@ model.outlet.static.df.loc[model.outlet.static.df.node_id == 74, "flow_rate"] = 
 model.outlet.static.df.loc[model.outlet.static.df.node_id == 289, "min_upstream_level"] = 12
 model.outlet.static.df.loc[model.outlet.static.df.node_id == 169, "min_upstream_level"] = -0.75
 
-# model.manning_resistance.static.df.loc[model.manning_resistance.static.df.loc[:,"manning_n"]]=0.005
+model.manning_resistance.static.df.loc[:, "manning_n"] = 0.005
 # %%
 
 # Write model
@@ -63,7 +64,7 @@ if run_model:
     exit_code = model.run()
     assert exit_code == 0
 
-    controle_output = Control(ribasim_toml=ribasim_toml)
+    controle_output = Control(ribasim_toml=ribasim_toml, qlr_path=qlr_path)
     indicators = controle_output.run_afvoer()
 
 # %%
