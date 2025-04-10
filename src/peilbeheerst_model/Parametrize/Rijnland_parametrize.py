77b0ed3d53da5050a739386d640b5365fa59c5ee
Hendrickx <Hendrickx@hkv.nl>
2025-04-10 17:38:01 +0200

    Clean-up of code: Rijnland.

--- src/peilbeheerst_model/Parametrize/Rijnland_parametrize.py
+++ src/peilbeheerst_model/Parametrize/Rijnland_parametrize.py
@@ -8,9 +8,6 @@ from ribasim import Node
 from ribasim.nodes import pump
 from shapely import Point
 
-# from ribasim import Node
-# from ribasim.nodes import level_boundary, tabulated_rating_curve
-# from shapely import Point
 import peilbeheerst_model.ribasim_parametrization as ribasim_param
 from peilbeheerst_model import supply
 from peilbeheerst_model.add_storage_basins import AddStorageBasins
@@ -29,10 +26,9 @@ cloud = CloudStorage()
 
 # collect data from the base model, feedback form, waterauthority & RWS border
 ribasim_base_model_dir = cloud.joinpath(waterschap, "modellen", f"{waterschap}_boezemmodel_{base_model_versie}")
-# FeedbackFormulier_path = cloud.joinpath(
-#     waterschap, "verwerkt", "Feedback Formulier", f"feedback_formulier_{waterschap}.xlsx"
-# )
-FeedbackFormulier_path = r"Z:\projects\4750_30\Ribasim_feedback\V1_formulieren\feedback_formulier_Rijnland.xlsx"
+FeedbackFormulier_path = cloud.joinpath(
+    waterschap, "verwerkt", "Feedback Formulier", f"feedback_formulier_{waterschap}.xlsx"
+)
 FeedbackFormulier_LOG_path = cloud.joinpath(
     waterschap, "verwerkt", "Feedback Formulier", f"feedback_formulier_{waterschap}_LOG.xlsx"
 )
@@ -44,7 +40,7 @@ aanvoer_path = cloud.joinpath(waterschap, "aangeleverd", "Na_levering", "Wateraa
 cloud.synchronize(
     filepaths=[
         ribasim_base_model_dir,
-        # FeedbackFormulier_path,
+        FeedbackFormulier_path,
         ws_grenzen_path,
         RWS_grenzen_path,
         qlr_path,
@@ -108,10 +104,10 @@ ribasim_param.validate_basin_area(ribasim_model)
 
 # model specific tweaks
 # merge basins
-ribasim_model.merge_basins(node_id=106, to_node_id=93, are_connected=True)  # klein gebied
-ribasim_model.merge_basins(node_id=235, to_node_id=151, are_connected=True)  # klein gebied
-ribasim_model.merge_basins(node_id=166, to_node_id=22, are_connected=True)  # klein gebied
-ribasim_model.merge_basins(node_id=79, to_node_id=22, are_connected=True)  # klein gebied
+ribasim_model.merge_basins(node_id=106, to_node_id=93, are_connected=True)  # (too) small area
+ribasim_model.merge_basins(node_id=235, to_node_id=151, are_connected=True)  # (too) small area
+ribasim_model.merge_basins(node_id=166, to_node_id=22, are_connected=True)  # (too) small area
+ribasim_model.merge_basins(node_id=79, to_node_id=22, are_connected=True)  # (too) small area
 # unconnected basins
 ribasim_model.merge_basins(node_id=308, to_node_id=22, are_connected=False)
 ribasim_model.merge_basins(node_id=332, to_node_id=138, are_connected=False)
@@ -134,7 +130,6 @@ ribasim_model.basin.area.df.loc[
 ribasim_model.basin.area.df.loc[ribasim_model.basin.area.df["meta_streefpeil"] == -9.999, "meta_streefpeil"] = str(
     unknown_streefpeil
 )
-
 
 # insert standard profiles to each basin: these are [depth_profiles] meter deep, defined from the streefpeil
 ribasim_param.insert_standard_profile(
@@ -211,9 +206,7 @@ ribasim_model.solver.saveat = saveat
 ribasim_model.write(ribasim_work_dir_model_toml)
 
 # run model
-ribasim_param.tqdm_subprocess(
-    [r"C:\ribasim_windows\ribasim\ribasim.exe", ribasim_work_dir_model_toml], print_other=False, suffix="init"
-)
+ribasim_param.tqdm_subprocess(["ribasim", ribasim_work_dir_model_toml], print_other=False, suffix="init")
 
 # model performance
 controle_output = Control(work_dir=work_dir, qlr_path=qlr_path)
