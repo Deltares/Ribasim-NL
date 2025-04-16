8de3fe2dde46efe54112764860aa86b4d6dc5d0c
Hendrickx <Hendrickx@hkv.nl>
2025-04-16 10:10:14 +0200

    Transferred implementation of continuous control for mixed conditions from temp. working file to 'main' parametrisation file for WSS.

--- src/peilbeheerst_model/Parametrize/Scheldestromen_parametrize.py
+++ src/peilbeheerst_model/Parametrize/Scheldestromen_parametrize.py
@@ -17,6 +17,7 @@ from peilbeheerst_model.ribasim_feedback_processor import RibasimFeedbackProcess
 from ribasim_nl import CloudStorage
 
 AANVOER_CONDITIONS: bool = True
+MIXED_CONDITIONS: bool = True
 
 # model settings
 waterschap = "Scheldestromen"
@@ -142,15 +143,28 @@ add_storage_basins = AddStorageBasins(
 
 add_storage_basins.create_bergende_basins()
 
-# set static forcing
-forcing_dict = {
-    "precipitation": ribasim_param.convert_mm_day_to_m_sec(0 if AANVOER_CONDITIONS else 10),
-    "potential_evaporation": ribasim_param.convert_mm_day_to_m_sec(10 if AANVOER_CONDITIONS else 0),
-    "drainage": ribasim_param.convert_mm_day_to_m_sec(0),
-    "infiltration": ribasim_param.convert_mm_day_to_m_sec(0),
-}
-
-ribasim_param.set_static_forcing(timesteps, timestep_size, starttime, forcing_dict, ribasim_model)
+# set forcing
+if MIXED_CONDITIONS:
+    halftime = starttime + (endtime - starttime) // 2
+    time = starttime, halftime, endtime
+    value = ribasim_param.convert_mm_day_to_m_sec(5)
+    precipitation = value, 0, 0
+    evaporation = 0, value, value
+    forcing_dict = {
+        "precipitation": precipitation,
+        "potential_evaporation": evaporation,
+        "drainage": 0,
+        "infiltration": 0,
+    }
+    ribasim_param.set_dynamic_forcing(ribasim_model, time, forcing_dict)
+else:
+    forcing_dict = {
+        "precipitation": ribasim_param.convert_mm_day_to_m_sec(0 if AANVOER_CONDITIONS else 10),
+        "potential_evaporation": ribasim_param.convert_mm_day_to_m_sec(10 if AANVOER_CONDITIONS else 0),
+        "drainage": ribasim_param.convert_mm_day_to_m_sec(0),
+        "infiltration": ribasim_param.convert_mm_day_to_m_sec(0),
+    }
+    ribasim_param.set_static_forcing(timesteps, timestep_size, starttime, forcing_dict, ribasim_model)
 
 # set pump capacity for each pump
 ribasim_model.pump.static.df["flow_rate"] = 0.16667  # 10 kuub per minuut
@@ -184,6 +198,7 @@ ribasim_param.find_upstream_downstream_target_levels(ribasim_model, node="pump")
 ribasim_param.set_aanvoer_flags(ribasim_model, str(aanvoer_path), processor, aanvoer_enabled=AANVOER_CONDITIONS)
 # ribasim_param.add_discrete_control(ribasim_model, waterschap, default_level)
 ribasim_param.determine_min_upstream_max_downstream_levels(ribasim_model, waterschap)
+ribasim_param.add_continuous_control(ribasim_model)
 
 # Manning resistance
 # there is a MR without geometry and without links for some reason
@@ -196,6 +211,8 @@ ribasim_model.manning_resistance.static.df.manning_n = 0.01
 # last formatting of the tables
 # only retain node_id's which are present in the .node table
 ribasim_param.clean_tables(ribasim_model, waterschap)
+if MIXED_CONDITIONS:
+    ribasim_model.basin.static.df = None
 
 # set numerical settings
 # write model output
