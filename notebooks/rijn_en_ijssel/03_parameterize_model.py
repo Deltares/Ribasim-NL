f804b2ea5abaa5acc273767cab276583fe13a75b
ngoorden <neeltje.goorden@gmail.com>
2025-04-18 15:40:41 +0200

    verbeteringen wrij

--- notebooks/rijn_en_ijssel/03_parameterize_model.py
+++ notebooks/rijn_en_ijssel/03_parameterize_model.py
@@ -8,7 +8,7 @@ cloud = CloudStorage()
 authority = "RijnenIJssel"
 short_name = "wrij"
 
-run_model = True
+run_model = False
 
 parameters_dir = static_data_xlsx = cloud.joinpath(authority, "verwerkt", "parameters")
 static_data_xlsx = parameters_dir / "static_data.xlsx"
@@ -20,6 +20,7 @@ ribasim_toml = ribasim_dir / f"{short_name}.toml"
 # # you need the excel, but the model should be local-only by running 01_fix_model.py
 cloud.synchronize(filepaths=[static_data_xlsx, profiles_gpkg], check_on_remote=False)
 cloud.synchronize(filepaths=[ribasim_dir], check_on_remote=False)
+qlr_path = cloud.joinpath("Basisgegevens\\QGIS_lyr\\output_controle_vaw_afvoer.qlr")
 
 # %%
 
@@ -32,8 +33,15 @@ start_time = time.time()
 model.parameterize(static_data_xlsx=static_data_xlsx, precipitation_mm_per_day=10, profiles_gpkg=profiles_gpkg)
 print("Elapsed Time:", time.time() - start_time, "seconds")
 
-# %%
+# %% fixes
+model.merge_basins(basin_id=944, to_basin_id=1028)
+model.merge_basins(basin_id=788, to_basin_id=1150)
+model.outlet.static.df.loc[model.outlet.static.df.node_id == 471, "active"] = False
+model.outlet.static.df.loc[model.outlet.static.df.node_id == 472, "active"] = False
+model.outlet.static.df.loc[model.outlet.static.df.node_id == 119, "min_upstream_level"] = 11
 
+
+# %%
 # Write model
 ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_parameterized_model", f"{short_name}.toml")
 model.write(ribasim_toml)
@@ -47,6 +55,6 @@ if run_model:
     assert exit_code == 0
 
     # # %%
-    controle_output = Control(ribasim_toml=ribasim_toml)
-    indicators = controle_output.run_all()
+    controle_output = Control(ribasim_toml=ribasim_toml, qlr_path=qlr_path)
+    indicators = controle_output.run_afvoer()
 # %%
