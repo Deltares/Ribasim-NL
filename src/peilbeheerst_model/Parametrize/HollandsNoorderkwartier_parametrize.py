c23d673c7d136b163161d412c017fc1dd34daf41
rbruijnshkv <r.bruijns@hkv.nl>
2025-04-25 14:45:09 +0200

    Fix locations validation issues

--- src/peilbeheerst_model/Parametrize/HollandsNoorderkwartier_parametrize.py
+++ src/peilbeheerst_model/Parametrize/HollandsNoorderkwartier_parametrize.py
@@ -182,6 +182,17 @@ pump_node = ribasim_model.pump.add(Node(geometry=Point(132040, 495282)), [pump.S
 ribasim_model.link.add(ribasim_model.basin[2], pump_node)
 ribasim_model.link.add(pump_node, level_boundary_node)
 
+# add hevelat Schermersluis (ARK-NZK --> boezem)
+level_boundary_node = ribasim_model.level_boundary.add(
+    Node(geometry=Point(111930, 494827)), [level_boundary.Static(level=[default_level])]
+)
+tabulated_rating_curve_node = ribasim_model.tabulated_rating_curve.add(
+    Node(geometry=Point(111934, 494910)),
+    [tabulated_rating_curve.Static(level=[0.0, 0.1234], flow_rate=[0.0, 0.1234])],
+)
+ribasim_model.link.add(level_boundary_node, tabulated_rating_curve_node)
+ribasim_model.link.add(tabulated_rating_curve_node, ribasim_model.basin[3])
+
 ribasim_model.merge_basins(node_id=201, to_node_id=79, are_connected=False)  # klein snippertje vlakbij IJsselmeer
 ribasim_model.merge_basins(node_id=192, to_node_id=17)  # klein snippertje vlakbij Markermeer
 ribasim_model.merge_basins(node_id=182, to_node_id=2)  # klein snippertje
