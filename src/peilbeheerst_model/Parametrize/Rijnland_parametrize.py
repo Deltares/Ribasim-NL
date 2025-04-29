a1cb74ba755c906e582d6557b10d6b07ab95ff01
Hendrickx <Hendrickx@hkv.nl>
2025-04-29 10:34:53 +0200

    CC-node implementation of Rijnland.

--- src/peilbeheerst_model/Parametrize/Rijnland_parametrize.py
+++ src/peilbeheerst_model/Parametrize/Rijnland_parametrize.py
@@ -16,8 +16,8 @@ from peilbeheerst_model.controle_output import Control
 from peilbeheerst_model.ribasim_feedback_processor import RibasimFeedbackProcessor
 from ribasim_nl import CloudStorage, Model
 
-AANVOER_CONDITIONS: bool = False
-MIXED_CONDITIONS: bool = False
+AANVOER_CONDITIONS: bool = True
+MIXED_CONDITIONS: bool = True
 
 if MIXED_CONDITIONS and not AANVOER_CONDITIONS:
     AANVOER_CONDITIONS = True
@@ -190,7 +190,7 @@ ribasim_param.find_upstream_downstream_target_levels(ribasim_model, node="outlet
 ribasim_param.find_upstream_downstream_target_levels(ribasim_model, node="pump")
 ribasim_param.set_aanvoer_flags(ribasim_model, aanvoergebieden, processor, aanvoer_enabled=AANVOER_CONDITIONS)
 ribasim_param.determine_min_upstream_max_downstream_levels(ribasim_model, waterschap)
-# ribasim_param.add_continuous_control(ribasim_model, dy=-50)
+ribasim_param.add_continuous_control(ribasim_model, dy=-50)
 
 # Manning resistance
 # there is a MR without geometry and without links for some reason
