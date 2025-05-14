54adc2c4e930fc85df568a1d55b208fb82fc8311
rbruijnshkv <r.bruijns@hkv.nl>
2025-05-14 22:09:00 +0200

    Use pyogrio instead of fiona as some columns were not loaded in

--- src/peilbeheerst_model/Data_prep_waterschappen/Wetterskip.py
+++ src/peilbeheerst_model/Data_prep_waterschappen/Wetterskip.py
@@ -17,14 +17,13 @@ data_delivery = cloud.joinpath(
 cloud.synchronize(filepaths=[data_delivery])
 
 
-def read_gpkg_layers(gpkg_path, engine="fiona", print_var=False):
+def read_gpkg_layers(gpkg_path, engine="pyogrio", print_var=False):
     data = {}
     layers = gpd.list_layers(gpkg_path)
     for layer in layers.name:
         if print_var:
-            print(layer)
-        data_temp = gpd.read_file(gpkg_path, layer=layer, engine=engine)
-        data[layer] = data_temp
+            data_temp = gpd.read_file(gpkg_path, layer=layer, engine=engine)
+            data[layer] = data_temp
 
     return data
 
