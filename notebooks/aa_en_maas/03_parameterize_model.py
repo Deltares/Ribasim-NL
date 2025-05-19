63e17e670d0505f0c8f3a57f94668ac5319f242b
Daniel Tollenaar <daniel@d2hydro.nl>
2025-05-19 11:07:30 +0200

    undo commit parameterize

---
+++ notebooks/aa_en_maas/03_parameterize_model.py
@@ -0,0 +1,60 @@
+import time
+
+from peilbeheerst_model.controle_output import Control
+from ribasim_nl import CloudStorage, Model
+from ribasim_nl.check_basin_level import add_check_basin_level
+
+cloud = CloudStorage()
+authority = "AaenMaas"
+short_name = "aam"
+
+run_model = False
+
+parameters_dir = static_data_xlsx = cloud.joinpath(authority, "verwerkt", "parameters")
+static_data_xlsx = parameters_dir / "static_data.xlsx"
+profiles_gpkg = parameters_dir / "profiles.gpkg"
+qlr_path = cloud.joinpath("Basisgegevens\\QGIS_lyr\\output_controle_vaw_afvoer.qlr")
+
+ribasim_dir = cloud.joinpath(authority, "modellen", f"{authority}_prepare_model")
+ribasim_toml = ribasim_dir / f"{short_name}.toml"
+
+# # you need the excel, but the model should be local-only by running 01_fix_model.py
+cloud.synchronize(filepaths=[static_data_xlsx, profiles_gpkg])
+cloud.synchronize(filepaths=[ribasim_dir], check_on_remote=False)
+
+# %%
+
+# read
+model = Model.read(ribasim_toml)
+
+start_time = time.time()
+
+
+# %%
+# parameterize
+
+model.parameterize(static_data_xlsx=static_data_xlsx, precipitation_mm_per_day=10, profiles_gpkg=profiles_gpkg)
+print("Elapsed Time:", time.time() - start_time, "seconds")
+
+# Fix afvoer
+model.outlet.static.df.loc[model.outlet.static.df.node_id == 375, "active"] = False
+
+# Write model
+
+add_check_basin_level(model=model)
+ribasim_toml = cloud.joinpath(authority, "modellen", f"{authority}_parameterized_model", f"{short_name}.toml")
+model.write(ribasim_toml)
+
+# %%
+
+# run model
+
+if run_model:
+    exit_code = model.run()
+    assert exit_code == 0
+
+    # # %%
+
+controle_output = Control(ribasim_toml=ribasim_toml, qlr_path=qlr_path)
+indicators = controle_output.run_afvoer()
+# %%
