910d782a2ba6de0d7aae31ba8f5ca7bdb5a69d48
Hendrickx <Hendrickx@hkv.nl>
2025-05-22 13:06:51 +0200

    Full implementation of dynamic forcing: HHNK.

--- src/peilbeheerst_model/Parametrize/HollandsNoorderkwartier_parametrize.py
+++ src/peilbeheerst_model/Parametrize/HollandsNoorderkwartier_parametrize.py
@@ -38,7 +38,9 @@ FeedbackFormulier_LOG_path = cloud.joinpath(
 )
 ws_grenzen_path = cloud.joinpath("Basisgegevens", "RWS_waterschaps_grenzen", "waterschap.gpkg")
 RWS_grenzen_path = cloud.joinpath("Basisgegevens", "RWS_waterschaps_grenzen", "Rijkswaterstaat.gpkg")
-qlr_path = cloud.joinpath("Basisgegevens", "QGIS_qlr", "output_controle_202502.qlr")
+qlr_path = cloud.joinpath(
+    "Basisgegevens", "QGIS_qlr", "output_controle_cc.qlr" if MIXED_CONDITIONS else "output_controle_202502.qlr"
+)
 aanvoer_path = cloud.joinpath(
     waterschap, "aangeleverd", "Na_levering", "20240618_peilgebieden_en_polders", "Polders_export_2024-06-18.shp"
 )
@@ -127,7 +129,6 @@ tabulated_rating_curve_node = ribasim_model.tabulated_rating_curve.add(
 )
 ribasim_model.link.add(ribasim_model.basin[3], tabulated_rating_curve_node)
 ribasim_model.link.add(tabulated_rating_curve_node, level_boundary_node)
-
 
 # add hevel near Andijk
 level_boundary_node = ribasim_model.level_boundary.add(
@@ -246,7 +247,10 @@ ribasim_param.Terminals_to_LevelBoundaries(ribasim_model=ribasim_model, default_
 ribasim_param.FlowBoundaries_to_LevelBoundaries(ribasim_model=ribasim_model, default_level=default_level)
 
 # add the default levels
-ribasim_model.level_boundary.static.df.level = default_level
+if MIXED_CONDITIONS:
+    ribasim_param.set_hypothetical_dynamic_level_boundaries(ribasim_model, starttime, endtime, -0.42, 0.42)
+else:
+    ribasim_model.level_boundary.static.df.level = default_level
 
 # add outlet
 ribasim_param.add_outlets(ribasim_model, delta_crest_level=0.10)
@@ -298,6 +302,8 @@ assign = AssignAuthorities(
     },
 )
 ribasim_model = assign.assign_authorities()
+if MIXED_CONDITIONS:
+    assign.from_static_to_time_df(ribasim_model, clear_static=True)
 
 # set numerical settings
 # write model output
