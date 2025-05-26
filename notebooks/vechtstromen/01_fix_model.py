c2d7975547ffd99ff3976cc3bde07b77c7c6fe15
Daniel Tollenaar <daniel@d2hydro.nl>
2025-05-26 12:44:37 +0200

    Update 01_fix_model.py

--- notebooks/vechtstromen/01_fix_model.py
+++ notebooks/vechtstromen/01_fix_model.py
@@ -1082,13 +1082,15 @@ for action in actions:
     print(action)
     # get method and args
     method = getattr(model, action)
-    keywords = inspect.getfullargspec(method).args
     df = gpd.read_file(model_edits_gpkg, layer=action, fid_as_index=True)
     if "order" in df.columns:
         df.sort_values("order", inplace=True)
     for row in df.itertuples():
         # filter kwargs by keywords
-        kwargs = {k: v for k, v in row._asdict().items() if k in keywords}
+        kwargs = row._asdict()
+        if inspect.getfullargspec(method).varkw != "kwargs":
+            keywords = inspect.getfullargspec(method).args
+            kwargs = {k: v for k, v in row._asdict().items() if k in keywords}
         method(**kwargs)
 
 # remove unassigned basin area
