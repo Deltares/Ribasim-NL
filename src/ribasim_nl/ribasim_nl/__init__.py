1bc53aa7508b837bde4f124e00ee1937d29dc3af
rbruijnshkv <r.bruijns@hkv.nl>
2025-06-18 11:48:34 +0200

    Add SetDynamicForcing class

--- src/ribasim_nl/ribasim_nl/__init__.py
+++ src/ribasim_nl/ribasim_nl/__init__.py
@@ -2,10 +2,21 @@ __version__ = "0.1.0"
 
 from ribasim_nl.cloud import CloudStorage
 from ribasim_nl.concat import concat
+from ribasim_nl.forcing import SetDynamicForcing
 from ribasim_nl.model import Model
 from ribasim_nl.network import Network
 from ribasim_nl.network_validator import NetworkValidator
 from ribasim_nl.reset_index import prefix_index, reset_index
 from ribasim_nl.settings import settings
 
-__all__ = ["CloudStorage", "concat", "Network", "reset_index", "prefix_index", "settings", "Model", "NetworkValidator"]
+__all__ = [
+    "CloudStorage",
+    "concat",
+    "SetDynamicForcing",
+    "Network",
+    "reset_index",
+    "prefix_index",
+    "settings",
+    "Model",
+    "NetworkValidator",
+]
