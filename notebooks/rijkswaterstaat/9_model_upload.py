0698a8e9e45cbe9dc77273225597c85e4c559114
Daniel Tollenaar <daniel@d2hydro.nl>
2025-09-23 15:42:26 +0200

    Run model after writing to cloud storage

--- notebooks/rijkswaterstaat/9_model_upload.py
+++ notebooks/rijkswaterstaat/9_model_upload.py
@@ -6,6 +6,8 @@ cloud = CloudStorage()
 ribasim_toml = cloud.joinpath("Rijkswaterstaat", "modellen", "hws_transient", "hws.toml")
 model = Model.read(ribasim_toml)
 model.write(cloud.joinpath("Rijkswaterstaat", "modellen", "hws", "hws.toml"))
+model.run()
 
+# %%
 cloud.upload_model("Rijkswaterstaat", "hws", include_results=True, include_plots=True)
 # %%
