1935bf6a131558ff2a1e741fcb01fe52d580e8f7
Maarten Pronk <git@evetion.nl>
2025-12-17 10:08:05 +0100

    Sync qlr path.

--- notebooks/aa_en_maas/03_parameterize_model.py
+++ notebooks/aa_en_maas/03_parameterize_model.py
@@ -20,7 +20,7 @@ ribasim_dir = cloud.joinpath(authority, "modellen", f"{authority}_prepare_model"
 ribasim_toml = ribasim_dir / f"{short_name}.toml"
 
 # # you need the excel, but the model should be local-only by running 01_fix_model.py
-cloud.synchronize(filepaths=[static_data_xlsx, profiles_gpkg])
+cloud.synchronize(filepaths=[static_data_xlsx, profiles_gpkg, qlr_path])
 cloud.synchronize(filepaths=[ribasim_dir], check_on_remote=False)
 
 # %%
