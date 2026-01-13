f45645cbc18d8a7b81e2f1e0159082b7ceced0dd
Hendrickx <Hendrickx@hkv.nl>
2026-01-13 10:46:49 +0100

    Added main-type function to label hydro-objects as main route.

--- src/ribasim_nl/profiles/path_finder.py
+++ src/ribasim_nl/profiles/path_finder.py
@@ -21,30 +21,32 @@ LOG = logging.getLogger(__name__)
 
 
 def fully_connected_network(
-    gdf: gpd.GeoDataFrame, *, buffer: float = 1e-2, reset_multi_index: bool = True
+    hydro_objects: gpd.GeoDataFrame, *, buffer: float = 1e-2, reset_multi_index: bool = True
 ) -> gpd.GeoDataFrame:
     # check for "real" MultiLineString-entries
-    ml = gdf[gdf.geometry.type == "MultiLineString"]
+    ml = hydro_objects[hydro_objects.geometry.type == "MultiLineString"]
     ml["n_linestring"] = ml.geometry.apply(lambda mls: len(mls.geoms))
     n_ml = sum(ml["n_linestring"] > 1)
     if n_ml > 0:
         LOG.warning("MultiLineString-entries found when generating a graph")
 
     # preprocess (Multi)LineString-entries
-    gdf = connect_linestrings_within_distance(gdf.explode(), buffer)
+    hydro_objects = connect_linestrings_within_distance(hydro_objects.explode(), buffer)
     if n_ml == 0:
-        gdf.index = gdf.index.droplevel(-1)
+        hydro_objects.index = hydro_objects.index.droplevel(-1)
     elif reset_multi_index:
-        gdf.reset_index(drop=True, inplace=True)
-    return gdf
+        hydro_objects.reset_index(drop=True, inplace=True)
+    return hydro_objects
 
 
-def generate_graph(gdf: gpd.GeoDataFrame, **kwargs) -> nx.Graph:
+def generate_graph(hydro_objects: gpd.GeoDataFrame, **kwargs) -> nx.Graph:
     # optional arguments
     columns: (str, ...) = kwargs.get("columns", ())
 
     # define data included in the network
-    network = gdf[[*columns, "geometry"]]
+    if "geometry" not in columns:
+        columns = *columns, "geometry"
+    network = hydro_objects[list(columns)]
 
     # generate graph
     graph = momepy.gdf_to_nx(network, length="weight")
@@ -95,13 +97,37 @@ def find_flow_routes(graph: nx.Graph, crossings: (shapely.Point, ...)) -> set[tu
     return flow_routes
 
 
-def get_flow_hydro_objects(
-    gdf: gpd.GeoDataFrame, graph: nx.Graph, flow_routes: set[tuple[tuple, tuple]], search_column: str = "geometry"
+def label_flow_hydro_objects(
+    hydro_objects: gpd.GeoDataFrame,
+    graph: nx.Graph,
+    flow_routes: set[tuple[tuple, tuple]],
+    search_column: str = "geometry",
 ) -> gpd.GeoDataFrame:
-    if search_column == "geometry":
-        selection = [data["geometry"] for nodes in flow_routes for data in graph.get_edge_data(*nodes).values()]
-    else:
-        selection = [graph.get_edge_data(*nodes)[search_column] for nodes in flow_routes]
+    routing_edges = [data[search_column] for nodes in flow_routes for data in graph.get_edge_data(*nodes).values()]
+    hydro_objects["main-route"] = hydro_objects[search_column].isin(routing_edges)
+    return hydro_objects
 
-    flow_hydro_objects = gdf[gdf[search_column].isin(selection)]
-    return flow_hydro_objects
+
+def label_main_routes(
+    basins: gpd.GeoDataFrame, hydro_objects: gpd.GeoDataFrame, crossings: gpd.GeoDataFrame, **kwargs
+) -> gpd.GeoDataFrame:
+    # optional arguments
+    buffer: float = kwargs.get("buffer", 1e-2)
+    reset_multi_index: bool = kwargs.get("reset_multi_index", True)
+    column_id: str = kwargs.get("column_id", "geometry")
+
+    # prepare hydro-objects: ensure hydro-objects are connected
+    hydro_objects = fully_connected_network(hydro_objects, buffer=buffer, reset_multi_index=reset_multi_index)
+
+    # generate network graph
+    graph = generate_graph(hydro_objects, columns=(column_id,))
+
+    # get crossings at basin-borders
+    border_crossings = select_crossings(shapely.MultiPolygon(basins.explode().geometry.values), crossings)
+
+    # find the shortest routes between border crossings
+    edge_nodes = find_flow_routes(graph, border_crossings)
+    main_routes = label_flow_hydro_objects(hydro_objects, graph, edge_nodes, search_column=column_id)
+
+    # return labelled hydro-objects
+    return main_routes
