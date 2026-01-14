708a9a3bcbd2c627b79c4fb4240a89b8b2dfd330
Hendrickx <Hendrickx@hkv.nl>
2026-01-14 11:22:45 +0100

    Added function docs.

--- src/ribasim_nl/profiles/bgt.py
+++ src/ribasim_nl/profiles/bgt.py
@@ -23,6 +23,26 @@ LOG = logging.getLogger(__name__)
 
 
 def download_bgt_water(geo_filter: shapely.Polygon | shapely.MultiPolygon = None, **kwargs) -> gpd.GeoDataFrame:
+    """Download BGT-data including only the 'waterdeel'-feature.
+
+    If no `geo_filter` is provided, the whole BGT-dataset is downloaded ('waterdeel'-feature only).
+
+    :param geo_filter: polygon specifying the region to be downloaded, defaults to None
+    :param kwargs: optional arguments
+
+    :key fn: filename to write the BGT-data to (if `wd` is defined), defaults to "bgt_water.gpkg"
+    :key sleep_time: time [seconds] between GET-requests for downloading the BGT-data, defaults to 1
+    :key wd: working directory to export the BGT-data to, defaults to None
+
+    :type geo_filter: shapely.Polygon | shapely.MultiPolygon, optional
+
+    :return: downloaded BGT-data
+    :rtype: geopandas.GeoDataFrame
+
+    :raises ValueError: if download request has failed
+    :raises ValueError: if download request is denied
+    :raises ValueError: if download failed
+    """
     # optional arguments
     fn: str = kwargs.get("fn", "bgt_water.gpkg")
     sleep_time: float = kwargs.get("sleep_time", 1)
@@ -72,13 +92,13 @@ def download_bgt_water(geo_filter: shapely.Polygon | shapely.MultiPolygon = None
     download_response = requests.get(download_url)
     LOG.debug(f"{download_response.status_code=}")
 
-    # download PDOK-data
+    # download BGT-data
     if download_response.status_code == 200:
         with zipfile.ZipFile(io.BytesIO(download_response.content)) as zip_file:
             (gml_fn,) = zip_file.namelist()
             with zip_file.open(gml_fn) as gml_file:
                 bgt_data = gpd.read_file(gml_file)
-                # > write PDOK-data
+                # > write BGT-data
                 if wd is not None:
                     bgt_data.to_file(wd / fn)
     else:
@@ -88,16 +108,32 @@ def download_bgt_water(geo_filter: shapely.Polygon | shapely.MultiPolygon = None
     return bgt_data
 
 
-def get_water_surfaces(
-    wd: pathlib.Path, geo_filter: shapely.Polygon | shapely.MultiPolygon, **kwargs
-) -> gpd.GeoDataFrame:
+def get_water_surfaces(wd: pathlib.Path, **kwargs) -> gpd.GeoDataFrame:
+    """Get water surfaces data, i.e., BGT-data.
+
+    If the data is not saved locally (i.e., `wd`) or `overwrite=True`, the data is downloaded via de PDOK API.
+
+    :param wd: working directory for BGT-data
+    :param kwargs: optional arguments
+
+    :key fn: filename to write the BGT-data to (if `write=True`), defaults to "bgt_water.gpkg"
+    :key geo_filter: polygon specifying the region to be downloaded, defaults to None
+    :key overwrite: overwrite the downloaded BGT-data with a new download, defaults to False
+    :key write: export the newly downloaded BGT-data to `wd`, defaults to True
+
+    :type wd: pathlib.Path
+
+    :return: BGT-data
+    :rtype: geopandas.GeoDataFrame
+    """
     # optional arguments
     fn: str = kwargs.get("fn", "bgt_water.gpkg")
-    force: bool = kwargs.get("force", False)
+    geo_filter: shapely.Polygon | shapely.MultiPolygon = kwargs.get("geo_filter")
+    overwrite: bool = kwargs.get("overwrite", False)
     write: bool = kwargs.get("write", True)
 
     # read pre-downloaded BGT-data
-    if (wd / fn).exists() and not force:
+    if (wd / fn).exists() and not overwrite:
         bgt_data = gpd.read_file(wd / fn)
         LOG.info(f"Used downloaded BGT-data: {wd / fn}")
     # download BGT-data
@@ -110,6 +146,24 @@ def get_water_surfaces(
 
 
 def upload_bgt_water(authority: str, cloud: CloudStorage = CloudStorage(), **kwargs) -> None:
+    """Upload BGT-data per water authority.
+
+    The geo-filter used for the download of the BGT-data is based on the basins of the water authority: A convex hull is
+    drawn around all basins to define the geo-filter.
+
+    :param authority: water authority
+    :param cloud: the GoodCloud-server, defaults to CloudStorage
+    :param kwargs: optional arguments
+
+    :key basins_fn: filename with water authority's basins, defaults to f'{authority}.gpkg'
+    :key basins_layer: layer-name of `basins_fn` with the basin-geometries, defaults to 'peilgebied'
+    :key mkdir: create the (local) directory to save the BGT-data to, defaults to True
+    :key overwrite: overwrite existing BGT-data, defaults to True
+    :key sync: sync the GoodCloud-server, defaults to True
+
+    :type authority: str
+    :type cloud: CloudStorage, optional
+    """
     # optional arguments
     basins_fn: str = kwargs.get("basins_fn", f"{authority}.gpkg")
     basins_layer: str = kwargs.get("basins_layer", "peilgebied")
@@ -138,7 +192,7 @@ def upload_bgt_water(authority: str, cloud: CloudStorage = CloudStorage(), **kwa
     fn_bgt = cloud.joinpath(authority, "verwerkt", "BGT", f"bgt_{authority}_water.gpkg")
     if mkdir:
         fn_bgt.parent.mkdir(parents=True, exist_ok=True)
-    _ = get_water_surfaces(fn_bgt.parent, geo_filter, fn=fn_bgt.name, force=overwrite, write=True)
+    _ = get_water_surfaces(fn_bgt.parent, geo_filter=geo_filter, fn=fn_bgt.name, force=overwrite, write=True)
 
     # upload BGT-data
     cloud.create_dir(authority, "verwerkt", "BGT")
