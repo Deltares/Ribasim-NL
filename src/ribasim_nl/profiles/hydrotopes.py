994dfd6aadc8ed40fee2e022e061566b1fabf697
Hendrickx <Hendrickx@hkv.nl>
2026-01-15 16:08:54 +0100

    Added interface-module for vdGaast hydrotopes.

--- src/ribasim_nl/profiles/hydrotopes.py
+++ src/ribasim_nl/profiles/hydrotopes.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 
 class HydrotopeTable:
-    collector: dict[int, "Hydrotope"]
+    def __init__(self, *hydrotope: "Hydrotope"):
+        self.collector: dict[int, Hydrotope] = {h.fid: h for h in hydrotope}
 
     def __getitem__(self, fid: int) -> "Hydrotope":
         if fid not in self.collector:
@@ -16,32 +17,6 @@ class HydrotopeTable:
 
     def __str__(self) -> str:
         return "\n".join(map(str, self.collector.values()))
-
-    @classmethod
-    def add(cls, hydrotope: "Hydrotope") -> "Hydrotope":
-        if hydrotope.fid in cls.collector:
-            msg = f"{hydrotope.fid=} already used: {cls.collector[hydrotope.fid]}"
-            raise KeyError(msg)
-
-        cls.collector.update({hydrotope.fid: hydrotope})
-
-        return hydrotope
-
-    @classmethod
-    def remove(cls, *, fid: int | None = None, hydrotope: "Hydrotope" | None = None) -> "Hydrotope":
-        assert (fid is None) ^ (hydrotope is None)
-        if hydrotope is not None:
-            fid = hydrotope.fid
-
-        if fid not in cls.collector:
-            msg = f"{fid=} not found; choose one of {(*cls.collector.keys(),)}"
-            raise KeyError(msg)
-
-        return cls.collector.pop(fid)
-
-    @classmethod
-    def reset(cls) -> None:
-        cls.collector.clear()
 
     @classmethod
     def from_csv(cls, fn: str, **kwargs) -> "HydrotopeTable":
@@ -60,18 +35,22 @@ class HydrotopeTable:
         data.astype({col_fid: int, col_name: str, **dict.fromkeys(cols_depths, float)})
 
         # initiate hydrotopes
-        data.apply(lambda row: Hydrotope(row[col_fid], row[col_name], (*row[cols_depths].values,)), axis=1)
+        hydrotypes = [
+            Hydrotope(fid, name, depths)
+            for _, (fid, name, *depths) in data[[col_fid, col_name, *cols_depths]].iterrows()
+        ]
+
+        # initiate tabe
+        table = cls(*hydrotypes)
 
         # return table
-        return cls()
+        return table
 
-    @classmethod
-    def get_by_fid(cls, fid: int) -> "Hydrotope":
-        return cls.collector[fid]
+    def get_by_fid(self, fid: int) -> "Hydrotope":
+        return self.collector[fid]
 
-    @classmethod
-    def get_by_name(cls, name: str) -> "Hydrotope":
-        (hydrotope,) = [h for h in cls.collector.values() if h.name == name]
+    def get_by_name(self, name: str) -> "Hydrotope":
+        hydrotope = next(h for h in self.collector.values() if h.name == name)
         return hydrotope
 
 
@@ -83,7 +62,6 @@ class Hydrotope:
 
     def __post_init__(self):
         assert len(self.depths) == 4
-        HydrotopeTable.add(self)
 
     def depth(self, width: float | None = None) -> tuple[float, float, float, float] | float:
         if width is None:
@@ -96,19 +74,3 @@ class Hydrotope:
         elif width < 6:
             return self.depths[2]
         return self.depths[3]
-
-
-def depth_by_name(name: str, width: float) -> float:
-    if name not in [h.name for h in HydrotopeTable.collector.values()]:
-        msg = f"Unknown {name=}"
-        raise KeyError(msg)
-
-    return HydrotopeTable.get_by_name(name).depth(width=width)
-
-
-def depth_by_fid(fid: int, width: float) -> float:
-    if fid not in HydrotopeTable.collector.keys():
-        msg = f"Unknown {fid=}"
-        raise KeyError(msg)
-
-    return HydrotopeTable.get_by_fid(fid).depth(width=width)
