059d892090a970d0d67c3e3ac1102ffb30275c46
Hendrickx <Hendrickx@hkv.nl>
2026-01-16 08:55:44 +0100

    Added function docs.

--- src/ribasim_nl/profiles/depth.py
+++ src/ribasim_nl/profiles/depth.py
@@ -6,13 +6,42 @@ from profiles import hydrotopes as ht
 def depth_from_hydrotopes(
     hydro_objects: gpd.GeoDataFrame, hydrotope_map: gpd.GeoDataFrame, hydrotopes: ht.HydrotopeTable, **kwargs
 ) -> gpd.GeoDataFrame:
+    """Determine representative depths of hydro-objects based on their representative width and hydrotope.
+
+    :param hydro_objects: geospatial data of hydro-objects
+    :param hydrotope_map: geospatial data of hydrotopes
+    :param hydrotopes: table with hydrotope-data
+    :param kwargs: optional arguments
+
+    :key col_fid: column-name with hydrotope ID data (in `hydrotope_map`), defaults to 'HYDROTOPE2'
+    :key drop_na: drop hydro-objects for which depth cannot be determined, defaults to True
+    :key min_map: minimal joining of `hydrotope_map` to the `hydro_objects`, defaults to True
+
+    :type hydro_objects: geopandas.GeoDataFrame
+    :type hydrotope_map: geopandas.GeoDataFrame
+    :type hydrotopes: profiles.hydrotopes.HydrotopeTable
+
+    :return: hydro-objects with depth-estimates
+    :rtype: geopandas.GeoDataFrame
+
+    :raises ValueError: if hydro-objects are missing width-estimates
+    """
+
+    def depth_calculator(fid: int, width: float) -> float | None:
+        """Calculation of depth based on overlapping hydrotope."""
+        ht = hydrotopes.get_by_fid(fid)
+        if ht is None:
+            return None
+        return ht.depth(width)
+
+    # verify required width estimations of hydro-objects
     if "width" not in hydro_objects.columns:
         msg = "Hydro-objects are missing width estimates"
         raise ValueError(msg)
 
     # optional arguments
     col_fid: str = kwargs.get("col_fid", "HYDROTYPE2")
-    dropna: bool = kwargs.get("dropna", True)
+    drop_na: bool = kwargs.get("drop_na", True)
     min_map: bool = kwargs.get("min_map", True)
 
     # verify definition of all occurring hydrotopes
@@ -26,28 +55,26 @@ def depth_from_hydrotopes(
     if min_map:
         hydrotope_map = hydrotope_map[[col_fid, "geometry"]]
 
-    def depth_calculator(fid: int, width: float) -> float | None:
-        ht = hydrotopes[fid]
-        if ht is None:
-            return None
-        return ht.depth(width)
-
     # align CRSs
     if hydro_objects.crs != hydrotope_map.crs:
         hydrotope_map.to_crs(hydro_objects.crs)
 
+    # find overlapping hydrotopes per hydro-object
     temp = gpd.sjoin(hydro_objects, hydrotope_map, how="left", predicate="intersects", rsuffix="map").reset_index(
         drop=False
     )
     temp["overlap"] = temp.apply(
         lambda row: row["geometry"].intersection(hydrotope_map.loc[row["index_map"], "geometry"]).length, axis=1
     )
+
+    # assign the hydrotope with the largest overlap per hydro-object
     idx = temp.groupby("index")["overlap"].idxmax()
     hydro_objects["ht_code"] = temp.loc[idx, col_fid]
 
-    print(f"{hydro_objects.columns=}")
+    # calculate depth
     hydro_objects["depth"] = hydro_objects.apply(lambda row: depth_calculator(row["ht_code"], row["width"]), axis=1)
-    if dropna:
+    if drop_na:
         hydro_objects.dropna(subset="depth", inplace=True, ignore_index=True)
 
+    # return updated hydro-objects
     return hydro_objects
