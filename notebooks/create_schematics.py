31b6b3915c6fb806465c9b93c6ae9a617d952107
ngoorden <neeltje.goorden@gmail.com>
2026-02-02 20:51:35 +0100

    nette plaatjes!

--- notebooks/create_schematics.py
+++ notebooks/create_schematics.py
@@ -12,8 +12,13 @@ from ribasim_nl import CloudStorage, Model
 cloud = CloudStorage()
 
 MODEL_POST_FIX = "dynamic_model"
-ARROW_LENGTH = 1200  # 800 voor HDSR #1000 voor nzv
-AUTHORITIES = ["StichtseRijnlanden", "AaenMaas"]
+ARROW_LENGTH = {"StichtseRijnlanden": 800, "Noorderzijlvest": 1200, "AaenMaas": 1400}
+OFFSETS = {
+    "AaenMaas": {"xmin": 3000, "ymin": 6000, "ymax": 5000},
+    "Noorderzijlvest": {"xmin": 20000, "ymin": 4500, "ymax": -7000},
+    "StichtseRijnlanden": {"xmin": 3000, "ymin": 2000, "ymax": 1000, "xmax": 1000},
+}
+AUTHORITIES = ["StichtseRijnlanden"]
 ADD_TOP10_NL = {
     "StichtseRijnlanden": [
         "Vecht",
@@ -49,6 +54,54 @@ ADD_TOP10_NL = {
         "Dommel",
         "Afwateringskanaal 's-Hertogenbosch - Drongelen",
     ],
+    "Noorderzijlvest": [
+        "Van Starkenborghkanaal",
+        "Lettelberterdiep",
+        "Leeksterhoofddiep",
+        "Jonkersvaart",
+        "Hoendiep",
+        "Aduarderdiep",
+        "Reitdiep",
+        "Winsumerdiep",
+        "Mensingeweersterloopdiep",
+        "Kromme Raken",
+        "Hoornse Vaart",
+        "Lauwersmeer",
+        "Broekstermaar",
+        "Westernielandstermaar",
+        "Molenrijgstermaar",
+        "Kommerzijlsterrijt",
+        "Hoerediep",
+        "Niezijlsterdiep",
+        "Wolddiep",
+        "Dwarsdiep",
+        "Matsloot",
+        "Boterdiep",
+        "Eemskanaal",
+        "Prinses Margrietkanaal|Casper Robles- of Kolonelsdiep",
+        "Dokkumerdiep",
+        "Zuider Ee",
+        "Dokkumergrootdiep",
+        "Oud Dokkumerdiep",
+        "Sauwerdermaar",
+        "Damsterdiep",
+        "Hunsingokanaal",
+        "Kommerzijlsterdiep",
+        "Warfhuisterloopdiep",
+        "Pieterbuurstermaar",
+        "Westernielandstermaar",
+        "Drentsche Hoofdvaart",
+        "Noord-Willemskanaal",
+        "Norgervaart",
+        "Hunzehaven",
+        "Zoutkamperril",
+        "Warffumermaar",
+        "Usquerdermaar",
+        "Oude Ae",
+        "Wetsingermaar",
+        "Kanaal Baflo-Mensingeweer",
+        "Rasquerdermaar",
+    ],
 }
 
 
@@ -65,7 +118,17 @@ def fix_geometry(geometry):
         return Polygon(geometry.exterior)
 
 
+def generate_mask_box(polygon: Polygon, offsets: dict["str", int]) -> Polygon:
+    xmin, ymin, xmax, ymax = polygon.bounds
+    xmin -= offsets.get("xmin", 5000)
+    ymin -= offsets.get("ymin", 5000)
+    xmax += offsets.get("xmax", 5000)
+    ymax += offsets.get("ymax", 5000)
+    return box(xmin, ymin, xmax, ymax)
+
+
 for authority in AUTHORITIES:
+    print(authority)
     # files we read
     sturing_dir = cloud.joinpath(authority, "verwerkt", "sturing")
     aanvoergebieden_gpkg = sturing_dir / "aanvoergebieden.gpkg"
@@ -84,12 +147,9 @@ for authority in AUTHORITIES:
     # maks layer
     df = gpd.read_file(water_authorities_gpkg)
     poly_mask = df.loc[df.nationalCode == waterbeheercode[authority], "geometry"].union_all()
-    xmin, ymin, xmax, ymax = poly_mask.bounds
-    xmin -= 10000
-    ymin -= 10000
-    xmax += 10000
-    ymax += 10000
-    mask_box = box(xmin, ymin, xmax, ymax)
+    mask_box = generate_mask_box(
+        poly_mask, OFFSETS.get(authority, {"xmin": 5000, "ymin": 5000, "xmax": 5000, "ymax": 5000})
+    )
     north_sea_df = gpd.read_file(north_see_gpkg, bbox=poly_mask.bounds)
     if not north_sea_df.empty:
         north_sea_poly = north_sea_df.union_all().buffer(100).buffer(-100)
@@ -137,6 +197,7 @@ for authority in AUTHORITIES:
 
     arrows = []
     for fid, row in structures_src_df.iterrows():
+        arrow_length = ARROW_LENGTH.get(authority, 1000)
         node_id = row.node_id
         if pd.isna(node_id):
             ValueError(f"node_id is NaN for fid {fid}. Search by name and code not implemented yet")
@@ -156,7 +217,7 @@ for authority in AUTHORITIES:
 
             structures_src_df.loc[fid, "functie"] = node_func
 
-        if not node_func == "geen":
+        if node_func != "geen":
             # get static data for next step
             node_type = connector_nodes_df.at[node_id, "node_type"]
             static_df = getattr(model, pascal_to_snake_case(node_type)).static.df
@@ -179,8 +240,8 @@ for authority in AUTHORITIES:
 
             # make arrow
             from_link = flow_links_df.set_index("from_node_id").at[node_id, "geometry"]
-            if from_link.length >= ARROW_LENGTH:
-                arrow = LineString([row.geometry, from_link.interpolate(ARROW_LENGTH)])
+            if from_link.length >= arrow_length:
+                arrow = LineString([row.geometry, from_link.interpolate(arrow_length)])
             else:
                 (x0, y0), (x1, y1) = (
                     (row.geometry.x, row.geometry.y),
@@ -189,7 +250,7 @@ for authority in AUTHORITIES:
                 dx = x1 - x0
                 dy = y1 - y0
                 current_length = (dx**2 + dy**2) ** (1 / 2)
-                scale = ARROW_LENGTH / current_length
+                scale = arrow_length / current_length
                 to_point = (x0 + dx * scale, y0 + dy * scale)
 
                 arrow = LineString([(x0, y0), to_point])
@@ -202,7 +263,8 @@ for authority in AUTHORITIES:
             structures_src_df.loc[fid, "code"] = connector_nodes_df.at[node_id, "meta_code_waterbeheerder"]
 
     structures_src_df.sort_values("functie").reset_index(drop=True).to_file(system_gpkg, layer="kunstwerken")
-    gpd.GeoDataFrame(arrows, crs=structures_src_df.crs).to_file(system_gpkg, layer="richting")
+    if len(arrows) > 0:
+        gpd.GeoDataFrame(arrows, crs=structures_src_df.crs).to_file(system_gpkg, layer="richting")
 
     mask = structures_src_df.add_label & ~structures_src_df.naam.isna() & structures_src_df.functie != "geen"
     table_md.write_text(
