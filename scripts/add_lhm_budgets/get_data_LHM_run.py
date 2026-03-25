dff9bb1ebbdbb2378a39afb4378e1e3d8c65cd63
Daniel Tollenaar <daniel@d2hydro.nl>
2026-03-25 09:43:47 +0100

    added TODO (i.o. Peter G)

--- scripts/add_lhm_budgets/get_data_LHM_run.py
+++ scripts/add_lhm_budgets/get_data_LHM_run.py
@@ -4,13 +4,24 @@ import imod
 import pandas as pd
 import zarr
 
-time_slice = slice(pd.Timestamp("2013-01-01"), pd.Timestamp("2022-12-31"))
+# TODO (for LHM):
+# - qrunm3 toevoegen door qrun te vermenigvuldingen met cellsize
+# - psswm3 toevoegen door pssw te vermenigvuldigen met cellsize
+# - metaswap resultaten (automatisch) fillen op de juiste tijdstap
+# - dxy automatisch bepalen op basis van modflow-rooster
+# - distance automatisch bepalen op basis van modflow-rooster (t.b.v. flexibele toepassing)
 
-path = Path(r"g:\projecten\2025\release_LHM433\modelruns\runs_LWKM\LWKM_run_resultaten_totaal")
+time_slice = slice(pd.Timestamp("2013-01-01"), pd.Timestamp("2022-12-31"))
+dxy = 250
+distance = 100_000
+
+base_path = Path(r"g:\projecten\2025\release_LHM433\modelruns\runs_LWKM\LWKM_run_resultaten_totaal")
+modflow_path = base_path / "modflow"
+metaswap_path = base_path / "metaswap"
 
 print("reading riv-budgets for sys 1")
 ar = (
-    imod.idf.open(path / "modflow" / "bdgriv/bdgriv_sys1_*_l*.IDF")
+    imod.idf.open(modflow_path / "bdgriv/bdgriv_sys1_*_l*.IDF")
     .sum(dim="layer")
     .drop_vars(["dy", "dx"])
     .sel(time=time_slice)
@@ -21,7 +32,7 @@ ds = ar.to_dataset()
 for isys in range(2, 7):
     print(f"reading riv-budgets for sys{isys}")
     ds[f"bdgriv_sys{isys}"] = (
-        imod.idf.open(path / "modflow" / f"bdgriv/bdgriv_sys{isys}_*_l*.IDF")
+        imod.idf.open(base_path / "modflow" / f"bdgriv/bdgriv_sys{isys}_*_l*.IDF")
         .sum(dim="layer")
         .drop_vars(["dy", "dx"])
         .sel(time=time_slice)
@@ -30,23 +41,18 @@ for isys in range(2, 7):
 for isys in range(1, 4):
     print(f"reading drn-budgets for sys {isys}")
     ds[f"bdgdrn_sys{isys}"] = (
-        imod.idf.open(path / "modflow" / f"bdgdrn/bdgdrn_sys{isys}_*_l*.IDF")
+        imod.idf.open(base_path / "modflow" / f"bdgdrn/bdgdrn_sys{isys}_*_l*.IDF")
         .sum(dim="layer")
         .drop_vars(["dy", "dx"])
         .sel(time=time_slice)
     )
 
 print("reading MetaSWAP budgets")
-bdgsw = imod.idf.open(path / "metaswap" / "bdgPssw/bdgPssw_*_l*.IDF").sum(dim="layer").drop_vars(["dy", "dx"])
-bdgqr = imod.idf.open(path / "metaswap" / "bdgQrun/bdgQrun_*_l*.IDF").sum(dim="layer").drop_vars(["dy", "dx"])
+bdgsw = imod.idf.open(metaswap_path / "bdgPssw/bdgPssw_*_l*.IDF").sum(dim="layer").drop_vars(["dy", "dx"])
+bdgqr = imod.idf.open(metaswap_path / "bdgQrun/bdgQrun_*_l*.IDF").sum(dim="layer").drop_vars(["dy", "dx"])
 ds["bdgpssw"] = bdgsw.resample(time="1D").bfill().sel(time=time_slice)
 ds["bdgqrun"] = bdgqr.resample(time="1D").bfill().sel(time=time_slice)
 
-# rechunk for better cloud access
-# spatial: 50km x 50 km
-# temporal: one year
-distance = 100_000
-dxy = 250
 n = int(distance / dxy)
 ds = ds.chunk(
     chunks={
