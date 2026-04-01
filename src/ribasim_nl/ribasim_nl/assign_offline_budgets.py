fixed surface_runoff sign (#512)
Fixed sign of surface_runoff budget. The budget should be negative (flux
leaving MetaSWAP) and thereby clipped on upper and made absolute as
Ribasim needs positive.

src/ribasim_nl/ribasim_nl/assign_offline_budgets.py
@@ -240,9 +240,9 @@ class AssignOfflineBudgets:
         infiltration = summed_budgets.clip(
             lower=0
         )  # alles > 0 (infiltratie is in modflow, ontrekking uit ribasim, maar in ribasim positief teken)
-        surface_runoff = pd.Series(budgets_df[list(surface_runoff_budgets)].sum(axis=1)).clip(
-            lower=0
-        )  # assume surface_runoff can't be <0 in RIBASIM
+        surface_runoff = (
+            pd.Series(budgets_df[list(surface_runoff_budgets)].sum(axis=1)).clip(upper=0).abs()
+        )  # assume surface_runoff can't be <0 in RIBASIM. And negative budgets in MODFLOW-MetaSWAP are positive terms in Ribasim
 
         # update basin drainage and infiltration
         idx = pd.MultiIndex.from_frame(model.basin.time.df[["node_id", "time"]])