2da87397ddb4c5ede8ccb5f2a5f70e77683d9319
rbruijnshkv <r.bruijns@hkv.nl>
2026-04-06 21:05:35 +0200

    Disable new profiles

--- src/peilbeheerst_model/Parametrize/WetterskipFryslan_parametrize.py
+++ src/peilbeheerst_model/Parametrize/WetterskipFryslan_parametrize.py
@@ -6,6 +6,7 @@ import warnings
 
 import peilbeheerst_model.ribasim_parametrization as ribasim_param
 import xarray as xr
+from peilbeheerst_model.add_storage_basins import AddStorageBasins
 from peilbeheerst_model.assign_authorities import AssignAuthorities
 from peilbeheerst_model.assign_parametrization import AssignMetaData
 from peilbeheerst_model.controle_output import Control
@@ -20,7 +21,6 @@ from ribasim_nl.control import (
     get_node_table_with_from_to_node_ids,
     set_node_functions,
 )
-from ribasim_nl.profiles import implement
 from shapely import Point
 
 from peilbeheerst_model import supply
@@ -476,20 +476,20 @@ for node_type in ["LevelBoundary", "TabulatedRatingCurve", "Pump"]:
     ribasim_model.node.df.loc[mask, "meta_node_id"] = ribasim_model.node.df.loc[mask].index
 
 # insert standard profiles to each basin: these are [depth_profiles] meter deep, defined from the streefpeil
-# ribasim_param.insert_standard_profile(
-#     ribasim_model,
-#     unknown_streefpeil=unknown_streefpeil,
-#     regular_percentage=regular_percentage,
-#     boezem_percentage=boezem_percentage,
-#     depth_profile=2,
-# )
+ribasim_param.insert_standard_profile(
+    ribasim_model,
+    unknown_streefpeil=unknown_streefpeil,
+    regular_percentage=regular_percentage,
+    boezem_percentage=boezem_percentage,
+    depth_profile=2,
+)
 
-# add_storage_basins = AddStorageBasins(
-#     ribasim_model=ribasim_model, exclude_hoofdwater=True, additional_basins_to_exclude=[]
-# )
-# add_storage_basins.create_bergende_basins()
+add_storage_basins = AddStorageBasins(
+    ribasim_model=ribasim_model, exclude_hoofdwater=True, additional_basins_to_exclude=[]
+)
+add_storage_basins.create_bergende_basins()
 
-implement.set_basin_profiles(ribasim_model, waterschap, cloud=cloud)
+# implement.set_basin_profiles(ribasim_model, waterschap, cloud=cloud)
 
 
 # set forcing
