52b6b9ee85789c01d19b0fc894d2f92563480bc2
rbruijnshkv <r.bruijns@hkv.nl>
2026-04-08 17:10:38 +0200

    temp changes

--- src/peilbeheerst_model/Parametrize/HollandseDelta_parametrize.py
+++ src/peilbeheerst_model/Parametrize/HollandseDelta_parametrize.py
@@ -8,6 +8,7 @@ import peilbeheerst_model.ribasim_parametrization as ribasim_param
 from peilbeheerst_model.assign_authorities import AssignAuthorities
 from peilbeheerst_model.assign_parametrization import AssignMetaData
 from peilbeheerst_model.controle_output import Control
+from peilbeheerst_model.outlet_pump_scaler import OutletPumpScalingConfig, scale_outlets_pumps
 from peilbeheerst_model.ribasim_feedback_processor import RibasimFeedbackProcessor
 from ribasim import Node, cli
 from ribasim.nodes import level_boundary, pump, tabulated_rating_curve
@@ -28,7 +29,7 @@ from ribasim_nl import CloudStorage, Model, SetDynamicForcing
 AANVOER_CONDITIONS: bool = True
 MIXED_CONDITIONS: bool = True
 DYNAMIC_CONDITIONS: bool = False
-RESCALE_FLOW_CAPACITIES: bool = True
+RESCALE_FLOW_CAPACITIES: bool = False
 
 if MIXED_CONDITIONS and not AANVOER_CONDITIONS:
     AANVOER_CONDITIONS = True
@@ -886,26 +887,43 @@ if MIXED_CONDITIONS:
     ribasim_model.basin.static.df = None
     ribasim_param.set_dynamic_min_upstream_max_downstream(ribasim_model)
 
+# IMPORTANT !!!!!!!!!!!!!!!!!!!!!! SET ribasim_model.pump.static.df["meta_known_flow_rate"] TO TRUE !!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 ribasim_model.outlet.static.df["meta_known_flow_rate"] = False
-ribasim_model.pump.static.df["meta_known_flow_rate"] = True
+ribasim_model.pump.static.df["meta_known_flow_rate"] = False
 ribasim_model.pump.static.df.loc[
     (ribasim_model.pump.static.df["max_flow_rate"].isna()) | (ribasim_model.pump.static.df["max_flow_rate"] == 0),
     "meta_known_flow_rate",
 ] = False
 
-# ribasim_model, from_to_node_table = scale_outlets_pumps(
-#     OutletPumpScalingConfig(
-#         ribasim_model_path=ribasim_work_dir_model_toml,
-#         ribasim_model=ribasim_model,
-#         from_to_node_function_table=from_to_node_function_table,
-#         waterschap=waterschap,
-#         cloud=cloud,
-#         rescale_flow_capacities=RESCALE_FLOW_CAPACITIES,
-#         max_iterations=2,
-#         design_precipitation_event=MIXED_CONDITIONS_DESIGN_P,
-#         design_potential_evaporation_event=MIXED_CONDITIONS_DESIGN_E,
-#     )
-# )
+# ribasim_model.pump.static.df.loc[ribasim_model.pump.static.df.flow_rate!= ribasim_model.pump.static.df.max_flow_rate]
+
+# WEGHALEN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ribasim_model.pump.static.df.max_flow_rate = 15
+ribasim_model.pump.static.df.flow_rate = 15
+
+ribasim_model, from_to_node_table = scale_outlets_pumps(
+    OutletPumpScalingConfig(
+        ribasim_model_path=ribasim_work_dir_model_toml,
+        ribasim_model=ribasim_model,
+        from_to_node_function_table=from_to_node_function_table,
+        waterschap=waterschap,
+        cloud=cloud,
+        rescale_flow_capacities=RESCALE_FLOW_CAPACITIES,
+        max_iterations=12,
+        design_precipitation_event=MIXED_CONDITIONS_DESIGN_P,
+        design_potential_evaporation_event=MIXED_CONDITIONS_DESIGN_E,
+    )
+)
+
+# WEGHALEN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ribasim_model.pump.static.df.max_flow_rate *= 1.1
+ribasim_model.outlet.static.df.max_flow_rate *= 1.1
+
 
 # add the water authority column to couple the model with
 assign = AssignAuthorities(
