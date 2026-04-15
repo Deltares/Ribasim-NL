aeec02646071d99001b60d1a8307907a96d6d6cb
Hendrickx <Hendrickx@hkv.nl>
2026-04-15 17:01:23 +0200

    Added HO-flagging implementation of profile generation.

--- src/peilbeheerst_model/profiles/gen_profiles.py
+++ src/peilbeheerst_model/profiles/gen_profiles.py
@@ -1,5 +1,7 @@
 """Generation of profiles."""
 
+import pathlib
+
 import geopandas as gpd
 import pandas as pd
 from ribasim_nl.profiles import run
@@ -55,7 +57,6 @@ def main(
     # > crossings, hydro-objects & target levels
     _fn_network = cloud.joinpath(water_authority, "verwerkt", "Crossings", fn_network)
     gdf_hydro_objects = gpd.read_file(_fn_network, layer="hydroobject")
-    # TODO: Catch for non-use of crossings?
     gdf_crossings = gpd.read_file(_fn_network, layer="crossings_hydroobject_filtered")
     gdf_target_levels = run.target_level_polygons(_fn_network)
     # > cross-sections
@@ -91,6 +92,96 @@ def main(
 
         # upload profile files
         print("Uploading to the GoodCloud...", end="", flush=True)
+        cloud.upload_content(wd_table, overwrite=True)
+        if export_intermediate_output:
+            cloud.upload_content(wd_table / "intermediate", overwrite=True)
+        print("Files uploaded to the GoodCloud")
+
+
+def flagged_hydro_objects(
+    water_authority: str,
+    fn_target_levels: str,
+    fn_hydro_objects: str | pathlib.Path,
+    col_flag: str,
+    *,
+    layer_hydro_objects: str = "hydroobjects",
+    export_profile_tables: bool = True,
+    sync: bool = True,
+    overwrite: bool = False,
+    export_intermediate_output: bool = False,
+) -> None:
+    """Execute profile table generator with user-defined main-routing.
+
+    This is a variant of `main(..)` in which the main route is predefined based on a flag in the hydro-objects.
+
+    :param water_authority: water authority
+    :param fn_hydro_objects: filename with geospatial data of hydro-objects
+        File is considered to be located at '<GoodCloud>/<water_authority>/fn_hydro_objects'
+    :param col_flag: column-name containing the main route flag
+    :param layer_hydro_objects: layer-name with hydro-objects data, defaults to "hydroobjects"
+    :param export_profile_tables: export generated profile tables, defaults to True
+    :param sync: sync with GoodCloud's 'verwerkt'- and 'Basisgegevens/Hydrotypen'-folders, defaults to True
+    :param overwrite: overwrite GoodCloud's 'verwerkt'- and 'Basisgegevens/Hydrotypen'-folders, defaults to False
+    :param export_intermediate_output: export intermediate output steps (for debugging), defaults to False
+    """
+    # get files from the cloud
+    cloud = CloudStorage()
+    if sync:
+        print("\rSyncing with the GoodCloud...", end="", flush=True)
+        cloud.download_verwerkt(water_authority, overwrite=overwrite)
+        cloud.download_basisgegevens(["Hydrotypen"], overwrite=overwrite)
+        cloud.download_file(cloud.joinurl(water_authority, str(fn_hydro_objects)))
+        print("Synced with the GoodCloud: 'Verwerkt', 'Basisgegevens/Hydrotypen' & `fn_hydro_objects`")
+
+    # read files
+    # > basins
+    fn_basins = cloud.joinpath(
+        water_authority, "verwerkt/Work_dir", f"{water_authority}_parameterized", "input/database.gpkg"
+    )
+    gdf_basins = gpd.read_file(fn_basins, layer="Basin / area")
+    # FIXME: Circular usage of basin-data
+    gdf_basins = gdf_basins[gdf_basins["node_id"] == gdf_basins["meta_node_id"]]
+    # > hydro-objects
+    gdf_hydro_objects = gpd.read_file(cloud.joinpath(water_authority, fn_hydro_objects), layer=layer_hydro_objects)
+    # > target levels
+    _fn_target_levels = cloud.joinpath(water_authority, "verwerkt", "Crossings", fn_target_levels)
+    gdf_target_levels = run.target_level_polygons(_fn_target_levels)
+    # > cross-sections
+    fn_cross_sections = cloud.joinpath(water_authority, "verwerkt", "profielen", "intermediate", "lines_z.gpkg")
+    gdf_cross_sections = gpd.read_file(fn_cross_sections)
+    # > BGT-data & hydrotopes
+    fn_bgt = cloud.joinpath(water_authority, "verwerkt", "BGT", f"bgt_{water_authority}_water.gpkg")
+    fn_hydrotopes = cloud.joinpath("Basisgegevens", "Hydrotypen", "vdGaast_water_depth.csv")
+
+    # execute profile generation
+    wd_int = (
+        cloud.joinpath(water_authority, "verwerkt", "profielen", "intermediate") if export_intermediate_output else None
+    )
+    profiles_tables = run.main(
+        gdf_basins,
+        gdf_hydro_objects,
+        gdf_cross_sections,
+        col_ho_main_route=col_flag,
+        target_levels=gdf_target_levels,
+        cloud=cloud,
+        fn_bgt=fn_bgt,
+        fn_hydrotopes=fn_hydrotopes,
+        export_intermediate_output=export_intermediate_output,
+        wd_intermediate_output=wd_int,
+    )
+
+    # export profile table
+    if export_profile_tables:
+        wd_table = cloud.joinpath(water_authority, "verwerkt", "profielen")
+        wd_table.parent.mkdir(exist_ok=True)
+        for table, name in zip(profiles_tables, ("doorgaand", "bergend")):
+            fn_table = wd_table / f"profielen_{name}.csv"
+            table = pd.DataFrame(table[[c for c in table.columns if c != "geometry"]])
+            table.to_csv(fn_table, index=False)
+            print(f"File saved: {fn_table}")
+
+        # upload profile files
+        print("\rUploading to the GoodCloud...", end="", flush=True)
         cloud.upload_content(wd_table, overwrite=True)
         if export_intermediate_output:
             cloud.upload_content(wd_table / "intermediate", overwrite=True)
