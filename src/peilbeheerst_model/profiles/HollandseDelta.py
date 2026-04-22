95c24532c2e19f218fa4985d21bf0c9fe2597795
Hendrickx <Hendrickx@hkv.nl>
2026-04-22 16:41:29 +0200

    HO-based profiles WSHD.

--- src/peilbeheerst_model/profiles/HollandseDelta.py
+++ src/peilbeheerst_model/profiles/HollandseDelta.py
@@ -3,10 +3,13 @@
 from profiles import gen_profiles
 
 if __name__ == "__main__":
-    # TODO: Hydro-object label-based
-    gen_profiles.main(
+    gen_profiles.flagged_hydro_objects(
         "HollandseDelta",
         "hd_crossings_v06.gpkg",
+        "aangeleverd/Eerste_levering/watergangen/HydroObjectWatergangtype.shp",
+        "SOORT_OPPE",
+        val_flag="hoofdwaterloop",
+        layer_hydro_objects=None,
         sync=False,
         export_profile_tables=True,
         export_intermediate_output=True,
