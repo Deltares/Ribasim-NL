439f87e741264d83deed8d9b0b7f95cc3ef1f96a
Hendrickx <Hendrickx@hkv.nl>
2026-04-22 16:07:55 +0200

    Added TODO: profiles based on HO-label.

--- src/peilbeheerst_model/profiles/HollandseDelta.py
+++ src/peilbeheerst_model/profiles/HollandseDelta.py
@@ -3,6 +3,7 @@
 from profiles import gen_profiles
 
 if __name__ == "__main__":
+    # TODO: Hydro-object label-based
     gen_profiles.main(
         "HollandseDelta",
         "hd_crossings_v06.gpkg",
