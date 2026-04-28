5d6686eb53f253a72e6162136445605b7b7bc983
Hendrickx <Hendrickx@hkv.nl>
2026-04-28 09:48:29 +0200

    Added lakes, etc. to overwrite rep. depth.

--- src/peilbeheerst_model/profiles/Rijnland.py
+++ src/peilbeheerst_model/profiles/Rijnland.py
@@ -3,10 +3,12 @@
 from profiles import gen_profiles
 
 if __name__ == "__main__":
+    fn_water_bodies = "aangeleverd/Na_levering/20260422_MerenPlassen/Rijnland_meren_plassen_diepte.geojson"
     gen_profiles.main(
         "Rijnland",
-        "rijnland_crossings_v04.gpkg",
+        "rijnland_crossings_v05.gpkg",
         sync=False,
         export_profile_tables=True,
         export_intermediate_output=True,
+        fn_water_bodies=fn_water_bodies,
     )
