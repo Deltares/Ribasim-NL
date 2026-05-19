df97906c28b216b695ce206f995df1fdef9ed512
Hendrickx <Hendrickx@hkv.nl>
2026-05-19 21:58:29 +0200

    Junctionfy HHNK.

--- src/peilbeheerst_model/Parametrize/HollandsNoorderkwartier_parametrize.py
+++ src/peilbeheerst_model/Parametrize/HollandsNoorderkwartier_parametrize.py
@@ -7,8 +7,8 @@ import peilbeheerst_model.ribasim_parametrization as ribasim_param
 import xarray as xr
 from peilbeheerst_model.assign_authorities import AssignAuthorities
 from peilbeheerst_model.assign_parametrization import AssignMetaData
-from peilbeheerst_model.basin_snapping import link_to_hydro_object, node_to_hydro_object_from_file
 from peilbeheerst_model.controle_output import Control
+from peilbeheerst_model.network_snapping import snap_model
 from peilbeheerst_model.outlet_pump_scaler import OutletPumpScalingConfig, scale_outlets_pumps
 from peilbeheerst_model.ribasim_feedback_processor import RibasimFeedbackProcessor
 from ribasim import Node, run_ribasim
@@ -27,7 +27,7 @@ from ribasim_nl.split_basins import NodeMetaCache, SplitBasins
 from shapely import Point
 
 from peilbeheerst_model import supply
-from ribasim_nl import CloudStorage, Model, Network, SetDynamicForcing, junctionify, merge_rwzi_model
+from ribasim_nl import CloudStorage, Model, SetDynamicForcing, junctionify, merge_rwzi_model
 
 AANVOER_CONDITIONS: bool = True
 MIXED_CONDITIONS: bool = True
@@ -71,13 +71,13 @@ splitted_basin_3_path = cloud.joinpath(waterschap, "verwerkt/Splitting_basins/Op
 
 cloud.synchronize(
     filepaths=[
-        # ribasim_base_model_dir,
-        # FeedbackFormulier_path,
-        # ws_grenzen_path,
-        # RWS_grenzen_path,
-        # qlr_path,
-        # aanvoer_path,
-        # meteo_path,
+        ribasim_base_model_dir,
+        FeedbackFormulier_path,
+        ws_grenzen_path,
+        RWS_grenzen_path,
+        qlr_path,
+        aanvoer_path,
+        meteo_path,
         profiles_path,
         splitted_basin_3_path,
     ]
@@ -253,7 +253,10 @@ node_cache.set_meta_category(ribasim_model)
 ribasim_model.write(ribasim_work_dir_model_toml)
 del node_cache
 
-# POTENTIAL DVC-BREAK
+# add junctions and network snapping
+if ADD_JUNCTIONS:
+    ribasim_model = snap_model(ribasim_model, profiles_path)
+    ribasim_model = junctionify(ribasim_model)
 
 # set basin profiles
 implement.set_basin_profiles(ribasim_model, waterschap, cloud=cloud, min_area=10)
@@ -620,13 +623,6 @@ if missing_meta_categorie_node_ids:
         f"Missing meta_categorie for basin node IDs: {missing_meta_categorie_node_ids}"
     )
 
-# add junctions
-if ADD_JUNCTIONS:
-    fn = profiles_path / "intermediate" / "int_output.gpkg"
-    ribasim_model = node_to_hydro_object_from_file(ribasim_model, fn, layer="hydro-objects", main_route_only=False)
-    network = Network.from_lines_gpkg(fn, layer="hydro-objects")
-    ribasim_model = link_to_hydro_object(ribasim_model, network, ("Basin",))
-    ribasim_model = junctionify(ribasim_model)
 # set numerical settings
 # write model output
 ribasim_model.use_validation = True
