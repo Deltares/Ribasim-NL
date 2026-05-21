c10a62a8c39786491ce43cf9ca6940e80e1e8569
Daniel Tollenaar <daniel@d2hydro.nl>
2026-05-21 21:49:05 +0200

    peilbeheerst en controle

---
+++ src/ribasim_nl/ribasim_nl/coupling/__init__.py
@@ -0,0 +1,6 @@
+from ribasim_nl.coupling.get_node_function import (
+    classify_node_by_control_name,
+    get_node_function,
+)
+
+__all__ = ["classify_node_by_control_name", "get_node_function"]
