171c5d6d11adb7662dd7ba40704d4d8d12ce57e7
Daniel Tollenaar <daniel@d2hydro.nl>
2026-06-04 13:12:02 +0200

    misc improvements

--- notebooks/rijn_en_ijssel/04_add_full_control.py
+++ notebooks/rijn_en_ijssel/04_add_full_control.py
@@ -109,6 +109,7 @@ outlet_max_flow_rate_by_node_id = {
     623: 10.3,  # Capaciteit volgens DM rapportage
     1192: 5.2,  # Capaciteit volgens DM rapportage
     560: 1.0,  # Capaciteit volgens DM rapportage
+    677: 0.05,  # Verdeelwerk Koppelleiding
 }
 
 # capaciteit inlaten
