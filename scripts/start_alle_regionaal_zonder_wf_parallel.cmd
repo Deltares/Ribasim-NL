5524a1d3c738204d09d6439c4d99045a97866d63
Daniel Tollenaar <daniel@d2hydro.nl>
2026-06-08 10:37:19 +0200

    remove pipelines

--- scripts/start_alle_regionaal_zonder_wf_parallel.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-
-call "%~dp0run_coupled_pipeline.cmd" alle-regionaal-zonder-wf-rws --start-at full-control --parallel-until-samenvoegen --parallel-new-windows %*
-exit /b %ERRORLEVEL%
--- scripts/start_dod_vecht_nzv_hunze_zonder_wf_parallel.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-
-call "%~dp0run_coupled_pipeline.cmd" dod-vecht-nzv-hunze-rws --start-at full-control --parallel-until-samenvoegen --parallel-new-windows %*
-exit /b %ERRORLEVEL%
